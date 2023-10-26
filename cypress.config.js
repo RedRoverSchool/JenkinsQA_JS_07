22b4bb15e49f72a9fd04219dd70ec0931fc71052
MaryMoroz <morozova.mary@gmail.com>
2023-10-25 23:57:03 -0400

    added all projects files

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require("cypress");
+
+module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 7000,
+  requestTimeout: 10000,
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
+    },
+  },
+});
