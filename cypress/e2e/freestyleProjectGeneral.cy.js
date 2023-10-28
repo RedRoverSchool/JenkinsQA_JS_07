184f16f76c9344aecb30af6df29b8d17f269f95b
Art Diuzhikov <115650565+adiuzhikov@users.noreply.github.com>
2023-10-28 15:53:51 -0400

    added new test Verify the user is able to disable the project and fixtures data (#12)

--- cypress/e2e/freestyleProjectGeneral.cy.js
+++ cypress/e2e/freestyleProjectGeneral.cy.js
@@ -14,4 +14,18 @@ describe('Freestyle Project > General', () => {
         cy.get('form h2#general').should('be.visible').and('have.text', freestyleProjectGeneral.sectionName);
         
     });
+
+    it('TC_04.01.003 | Verify the user is able to disable the project', function() {
+
+        cy.get('#side-panel a[href$=newJob]').click();
+        cy.get('form input#name').type('FreestyleProject1');
+        cy.get('form #items li[class$=FreeStyleProject]').click();
+        cy.get('form button[type=submit]').click();
+
+        cy.get('form input[class*=toggle-switch]').should('be.enabled');
+      
+        cy.get('form label[class*=toggle-switch]').click().should('not.be.enabled');
+        cy.get('form span[class*=unchecked-title]').should('have.text', freestyleProjectGeneral.statusDisabled).and('be.visible');
+        
+      });
 });
