1c802d2f6b6ee69598e43090e5fbfe4465615536
Oxana Reshes <oxanareshes@gmail.com>
2023-10-27 23:56:24 -0700

    added test verify Jenkins Logo and Title

---
+++ cypress/e2e/logoandTitle.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('Logo and Title', () => {
+
+    it('verify Jenkins Logo and Title', function () {
+      cy.get('#jenkins-home-link').should('be.visible').click()
+    })
+    it('verify if Icon “Jenkins” is an image', function () {
+      cy.get('img[src="/static/0d56e664/images/title.svg"]').should('be.visible');
+    
+    })
+})
