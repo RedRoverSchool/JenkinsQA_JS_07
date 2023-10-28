762642911749504ca02d094299594c3c63babf7b
Ksenyia Kravchuk <kkravchuk@uma.tech>
2023-10-27 22:22:11 -0400

    Changes according comments

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -1,9 +1,9 @@
 /// <reference types="cypress"/>
 
-describe('verify welcomed message exist', () => {
+describe('dashboard', () => {
     const message = 'Welcome to Jenkins!';
 
-    it('welcomed message exist', () => {
+    it('TC_02.03.001 | Dashboard > Check welcomed message', () => {
         cy.get('.empty-state-block h1').should('exist').should('have.text', message)
     })
 });
