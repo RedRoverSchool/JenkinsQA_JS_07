9319457916091c8e26cd805ec88313f51ace191e
NadyaShulga <shulgaqa@gmail.com>
2023-10-29 17:03:59 -0400

    chandes in code

--- cypress/e2e/iconJenkinsTest.cy.js
+++ cypress/e2e/iconJenkinsTest.cy.js
@@ -3,8 +3,8 @@
 describe ('iconJenkins', () =>{
 
     it('Verify icon and logo on the first page', function (){
-cy.get('#jenkins-head-icon').should('exist').click().should('be.visible');
-cy.get('#jenkins-name-icon').should('exist').click().should('be.visible');
+cy.get('#jenkins-head-icon').click().should('be.visible');
+cy.get('#jenkins-name-icon').click().should('be.visible');
 
     })
 })
