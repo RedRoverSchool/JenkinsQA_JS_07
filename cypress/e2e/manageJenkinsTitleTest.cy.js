357752a12f5df4a90427e85b3a510d877608c6af
RuslanG13 <ruslan.gudenko13@ukr.net>
2023-10-29 03:39:13 +0200

    fix locator for Manage Jenkins item menu

--- cypress/e2e/manageJenkinsTitleTest.cy.js
+++ cypress/e2e/manageJenkinsTitleTest.cy.js
@@ -6,7 +6,7 @@ import manageJenkinsTitle from "../fixtures/manageJenkinsTitle.json";
 describe("Manage Jenkins > Title", () => {
 
     it("TC_09.01.001 | Manage Jenkins > Title > Main header displays verification", () => {
-        cy.get(".task-link-wrapper  a[href='/manage']").click();
+        cy.get(".task-link-wrapper > a[href='/manage']").click();
 
         cy.get(".jenkins-app-bar__content>h1").should("be.visible")
             .and("have.text", manageJenkinsTitle.mainHeaderPage);
