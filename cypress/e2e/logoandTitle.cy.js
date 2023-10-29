97402fe7b57974f9809d2450ecbb7ffcd75a0084
Oxana Reshes <oxanareshes@gmail.com>
2023-10-28 19:03:45 -0700

    added test Logo and Title

--- cypress/e2e/logoandTitle.cy.js
+++ cypress/e2e/logoandTitle.cy.js
@@ -6,7 +6,6 @@ describe('Logo and Title', () => {
       cy.get('#jenkins-home-link').should('be.visible').click()
     })
     it('verify if Icon “Jenkins” is an image', function () {
-      cy.get('img[src="/static/0d56e664/images/title.svg"]').should('be.visible');
-      cy.get('#jenkins-head-icon').should('be.visible').and('have.attr','src')
-     })
+      cy.get('#jenkins-head-icon').should('have.css', 'background-image');
+        })
 })
