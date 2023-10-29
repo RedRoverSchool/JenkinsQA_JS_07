dadc9563dad9f473fbf948705ec04f53951617c4
RuslanG13 <ruslan.gudenko13@ukr.net>
2023-10-29 03:41:05 +0200

    fix locator for main title

--- cypress/e2e/manageJenkinsTitleTest.cy.js
+++ cypress/e2e/manageJenkinsTitleTest.cy.js
@@ -8,7 +8,7 @@ describe("Manage Jenkins > Title", () => {
     it("TC_09.01.001 | Manage Jenkins > Title > Main header displays verification", () => {
         cy.get(".task-link-wrapper > a[href='/manage']").click();
 
-        cy.get(".jenkins-app-bar__content>h1").should("be.visible")
+        cy.get(".jenkins-app-bar__content > h1").should("be.visible")
             .and("have.text", manageJenkinsTitle.mainHeaderPage);
     });
 }); 
