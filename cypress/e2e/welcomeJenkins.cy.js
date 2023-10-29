e57255cf3017fead3bd3fce470ce563f014f30fb
NadyaShulga <shulgaqa@gmail.com>
2023-10-29 17:26:33 -0400

    changes in code

--- cypress/e2e/welcomeJenkins.cy.js
+++ cypress/e2e/welcomeJenkins.cy.js
@@ -5,7 +5,7 @@ describe ('welcomeJenkins', () =>{
     it('"Welcome to Jenkins" title and text', function (){
 
 cy.get('h1').should('be.visible').and('have.text', 'Welcome to Jenkins!').hasAttribute('h1');
-cy.get('')
+cy.get('h1')
     })
 
 })
