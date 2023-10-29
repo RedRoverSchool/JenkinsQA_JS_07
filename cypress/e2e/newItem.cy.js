510beb16321cb08aa9491b40269b190aa5dada53
Sviatlana Bannikova <atevs2789@gmail.com>
2023-10-29 10:30:07 -0500

    typesOfProjects (#18)
    Co-authored-by: Sviatlana Bannikova <sbanni@Sviatlanas-MacBook-Air.local>

--- cypress/e2e/newItem.cy.js
+++ cypress/e2e/newItem.cy.js
@@ -2,13 +2,24 @@
 
 import newJobPage from "../fixtures/newJobPage.json"
 
+
 describe('newItem', () => {
 
-    it('TC_03.01.001 | + New Item > Verify that user after click on +New Item option is redirected to the "New job" page', function() {
+    it('TC_03.01.001 | + New Item > Verify that user after click on +New Item option is redirected to the "New job" page', function () {
 
         cy.get('[href*="/view/all/newJob"]').click()
 
         cy.url().should('include', 'view/all/newJob')
-        cy.get('.add-item-name .h3').should('have.text',newJobPage.newJobPageFieldName)
+        cy.get('.add-item-name .h3').should('have.text', newJobPage.newJobPageFieldName)
     })
-})
+
+    it('TC_03.01.002| + New Item> Verify that the following types of projects: “Freestyle project”, “Pipeline”, “Multi-configuration project”, “Folder”, “Multibranch Pipeline”, “Organization Folder “ are displayed', () => {
+
+        cy.get('[href*="/view/all/newJob"]').click()
+
+        cy.get('.label').each(($element, index) => {
+            cy.wrap($element).should('have.text', newJobPage.expectedProjectLabels[index]).and('be.visible');
+        });
+    });
+
+});
