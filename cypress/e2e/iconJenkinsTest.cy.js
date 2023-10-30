0aa41df5cad9f7ba48c33c29b570ed49ffda6a58
NadyaShulga <shulgaqa@gmail.com>
2023-10-29 20:11:59 -0400

    new code

--- cypress/e2e/iconJenkinsTest.cy.js
+++ cypress/e2e/iconJenkinsTest.cy.js
@@ -1,10 +1,11 @@
 ///<reference types ="cypress"/>
 
-describe ('iconJenkins', () =>{
+describe ('iconJenkinsTest', () =>{
 
-    it('Verify icon and logo on the first page', function (){
+    it('TC_02.02.001 | Verify icon and logo on the first page', function (){
+        
 cy.get('#jenkins-head-icon').click().should('be.visible');
 cy.get('#jenkins-name-icon').click().should('be.visible');
 
-    });
+    })
 })
--- cypress/e2e/welcomeJenkins.cy.js
+++ cypress/e2e/welcomeJenkins.cy.js
@@ -2,11 +2,11 @@
 
 describe ('welcomeJenkins', () =>{
 
-    it('"Welcome to Jenkins" title and text', function (){
+    it('TC_02.03.004 | "Welcome to Jenkins" title and text', function (){
 
 cy.get('h1').should('be.visible').and('have.text', 'Welcome to Jenkins!');
 cy.get('div.empty-state-block p').should('exist').and('contain','This page is where your Jenkins jobs will be displayed.')
 
-    })
+    });
 
 })
