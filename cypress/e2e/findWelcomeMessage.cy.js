a3b37e23bd9024ad2bc906e2c171b646f06a97b3
AlexeyMartynov <infloor@gmail.com>
2023-10-30 16:21:59 -0500

    replace "" with ''

--- cypress/e2e/findWelcomeMessage.cy.js
+++ cypress/e2e/findWelcomeMessage.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 
 describe('findWelcomeMessage',() => {
-    it("findWelcomeMessage", () => {
+    it('findWelcomeMessage', () => {
         cy.get('.empty-state-block h1').should('be.visible').and('have.text','Welcome to Jenkins!')
     }) 
 })
