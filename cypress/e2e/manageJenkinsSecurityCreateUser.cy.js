5865ad0c89d719daf0760ae2c9e362ea3999c89e
Dzina Barabash <dzina_barabash@epam.com>
2023-10-31 13:59:02 -0400

    TC_09.14.002 | Create user with empty username

--- cypress/e2e/manageJenkinsSecurityCreateUser.cy.js
+++ cypress/e2e/manageJenkinsSecurityCreateUser.cy.js
@@ -27,4 +27,20 @@ describe('ManageJenkinsSecurityCreateUser.cy', () => {
       manageJenkinsSecurityCreateUser.username
     );
   });
+
+  it.only('Manage Jenkins > Security > Create user > Verify error message displayed when creating user without username', () => {
+    cy.get('a[href="/manage"]').click();
+    cy.title().should('include', 'Manage Jenkins');
+    cy.get('a[href="securityRealm/"]').click();
+    cy.title().should('include', 'Users');
+    cy.get('a[href="addUser"]').click();
+    cy.title().should('include', 'Create User');
+    cy.get('#username').should('not.have.value');
+    cy.get('input[name="password1"]').type(manageJenkinsSecurityCreateUser.password);
+    cy.get('input[name="password2"]').type(manageJenkinsSecurityCreateUser.password);
+    cy.get('input[name="fullname"]').type(manageJenkinsSecurityCreateUser.fullName);
+    cy.get('input[name="email"]').type(manageJenkinsSecurityCreateUser.email);
+    cy.get('button[name="Submit"]').click();
+    cy.get('div.error').should('contain', '""').and('contain', manageJenkinsSecurityCreateUser.errorUsername);
+  });
 });
