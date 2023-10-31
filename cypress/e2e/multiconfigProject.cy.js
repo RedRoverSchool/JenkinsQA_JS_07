834496cf371d85d6849751544cd9a4f27c1020a2
Dymaretskii-Dmitrii <141055198+GitDym@users.noreply.github.com>
2023-10-31 22:10:00 +0200

    Update multiconfigProject.cy.js

--- cypress/e2e/multiconfigProject.cy.js
+++ cypress/e2e/multiconfigProject.cy.js
@@ -11,14 +11,4 @@ describe('multiconfigProject', () => {
 
         cy.get('td a[href*="job"].jenkins-table__link').should('have.text', 'Project1')
     })
-
-//     it('TC_03.06.006 | Create Multiconfiguration project', () => {
-//         cy.get('a[href="/view/all/newJob"]').click()
-//         cy.get('input#name').type('Project1')
-//         cy.get('li[tabindex="0"] span').contains('Multi-configuration project').click()
-//         cy.get('#ok-button').click()
-//         cy.get('button').contains('Save').click()
-//         cy.get('.jenkins-breadcrumbs__list-item:nth-child(1) > .model-link').click()
-//         cy.get('td a[href*="job"].jenkins-table__link').should('have.text', 'Project1')
     })
-// })
