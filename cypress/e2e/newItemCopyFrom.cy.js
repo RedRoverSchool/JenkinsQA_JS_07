0a2faf528a877775ed2f6165e1edb65c9796688d
Ksenyia Kravchuk <kkravchuk@uma.tech>
2023-10-30 22:40:09 -0400

    TC_03.08.002 | New Item > Copy from > Copy from field exist

--- cypress/e2e/newItemCopyFrom.cy.js
+++ cypress/e2e/newItemCopyFrom.cy.js
@@ -15,4 +15,9 @@ describe('New item > Copy from', () => {
         cy.get('a[href="/view/all/newJob"]').click();
         cy.url().should('be.equal', data.newItemPage)
     });
+
+    it('TC_03.08.002 | New Item > Copy from > "Copy from" field exist', () => {
+        cy.get('a[href="/view/all/newJob"]').click();
+        cy.get('div.item-copy').should('be.exist').and('be.visible');
+    });
 });
