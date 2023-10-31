6abadfde1d81643141ef0238d078bd2cdb5f38ce
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-10-31 13:09:30 -0400

    Add new test

---
+++ cypress/e2e/newItemInCommandPanel.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress"/>
+
+describe('Dashboard > Verify "New Item" in Command panel.', () => {
+    it('Verify "New Item" link redirect to "Enter an item name" page', () => {
+        cy.get('a[href="/view/all/newJob"]')
+        .should('have.text', 'New Item').click();
+        cy.url().should('be.equal', 'http://localhost:8080/view/all/newJob');
+        cy.get('div.header').should('be.visible').and('have.text', 'Enter an item name')
+    });
+});
