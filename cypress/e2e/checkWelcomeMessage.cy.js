1ba55f1486f31998189b901255484f0f0acc5ac8
EKlipert <klipert.kate@gmail.com>
2023-10-31 21:52:36 +0100

    TC_02.03.007 | Dashboard > Welcome message. Changed test and describe names, merged tests in 1

--- cypress/e2e/checkWelcomeMessage.cy.js
+++ cypress/e2e/checkWelcomeMessage.cy.js
@@ -1,15 +1,9 @@
 /// <reference types="cypress"/>
 
-describe("TC_02.03.007 | Dashboard > Welcome message", () => {
-  it("Verify the URL", () => {
+describe("checkWelcomeMessage", () => {
+  it("Dashboard > Welcome message", () => {
     cy.url().should("be.equal", "http://localhost:8080/");
-  });
-
-  it("Verify h1 contains welcome message", () => {
     cy.get("div h1").contains("Welcome to Jenkins!").should("be.visible");
-  });
-
-  it("Verify the title under h1 contains explaining message", () => {
     cy.get("div h1")
       .next("p")
       .contains(
