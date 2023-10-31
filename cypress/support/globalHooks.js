f497a1cce12dbe8c984b5cf9f91638d43de89807
Sergey Kuznetsov <votkinsk@gmail.com>
2023-10-30 18:20:39 -0700

    Add HOST env variable

--- cypress/support/globalHooks.js
+++ cypress/support/globalHooks.js
@@ -1,10 +1,11 @@
 const USERNAME = Cypress.env('local.admin.username');
 const PASSWORD = Cypress.env('local.admin.password');
-const LOCAL_PORT = Cypress.env('local.port')
+const PORT = Cypress.env('local.port');
+const HOST = Cypress.env('local.host') || 'localhost';
 
 beforeEach(() => {
     cy.cleanData();
-    cy.visit(`http://localhost:${LOCAL_PORT}/login`);
+    cy.visit(`http://${HOST}:${PORT}/login`);
       cy.get('#j_username').type(USERNAME);
       cy.get('input[name="j_password"]').type(PASSWORD);
       cy.get('button[name="Submit"]').click();
