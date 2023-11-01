2c643556e5d32391079f5907ea82e93e40415afe
YuriySEM <yuko40118@gmail.com>
2023-11-01 16:42:29 -0500

    fix TC name, add assertions for titles

--- cypress/e2e/newItem.cy.js
+++ cypress/e2e/newItem.cy.js
@@ -30,8 +30,13 @@ describe('newItem', () => {
         cy.get('#ok-button').should('have.class', 'disabled');
     });
 
-    it('TC_03.01.005| + New Item> Verify +New Item functionality', () => {
+    it('TC_03.01.005| + New Item> Verify +New Item titles are displayed', () => {
         cy.get('[href*="/view/all/newJob"]').click();
         cy.url().should('eql', newItem.newItemPage);
+        cy.get('.add-item-name label').should('have.text', newJobPage.fieldName);
+        cy.get('span.label').each(($el, i) => {
+            cy.wrap($el).should('have.text',newJobPage.expectedProjectLabels[i]);
+        });
+
     });
 });
