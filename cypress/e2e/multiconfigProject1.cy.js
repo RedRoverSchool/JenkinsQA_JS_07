47da1ca4766114bfe1f78e6a03f300eae7174911
emmashabsis <146040143+emmashabsis@users.noreply.github.com>
2023-11-01 14:08:02 -0700

    added test multiconfigProject1

--- cypress/e2e/multiconfigProject1.cy.js
+++ cypress/e2e/multiconfigProject1.cy.js
@@ -1,12 +1,16 @@
-///reference types="cypress"/>
-
+/// <reference types="cypress"/>
 describe('multiconfigProject1', () => {
-    it('Create Multiconfiguration project', function() {
+
+    it('Create multiconfigProject1', function() {
         cy.get('a[href="newJob"]').click()
         cy.get('input#name').type('Project1')
         cy.get('li[tabindex="0"] span').contains('Multi-configuration project').click()
         cy.get('#ok-button').click()
-        cy.get('.jenkins-breadcrumbs_list-item:nth-child(1)>.model-link').click()
-        cy.get('td a[href*="job"].jenkins-table_link').should('have.text', 'Project1')
+        cy.get('.jenkins-breadcrumbs__list-item:nth-child(1) > .model-link').click()
+
+        cy.get('td a[href*="job"].jenkins-table__link').should('have.text', 'Project1')
     })
-});
+    })
+
+
+
