0f5617a565cdc482b189f664e15a4c8327f32554
Elena Pakhomova <pakhomova.e@gmail.com>
2023-11-01 19:04:52 +0300

    added test TC_03.04.005 | Create Pipeline project > Not unique name

---
+++ cypress/e2e/createPipelineProjectNewItem.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+
+let nameProject= "firstProject"
+
+describe('createPiPelineProjectNewItem', () => {
+
+    it('TC_03.04.005 | Create Pipeline project > Not unique name', () => {
+        cy.visit('http://localhost:8080/')
+        cy.get("a[href='/view/all/newJob']").click()
+        cy.get('input#name').type(nameProject)
+        cy.get('li[tabindex="0"] span').contains('Pipeline').click()
+        cy.get('#ok-button').click()
+        cy.get('a[href="/"].model-link').click()
+        cy.get("a[href='/view/all/newJob']").click()
+        cy.get('input#name').type(nameProject)
+        cy.get('li[tabindex="0"] span').contains('Pipeline').click()
+
+        cy.get('#ok-button').should('be.disabled')
+    })
+})
