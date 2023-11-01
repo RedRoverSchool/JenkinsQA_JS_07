4d23f4cdd8aaa0bf559b202e6523de08f43d7769
Art Diuzhikov <115650565+adiuzhikov@users.noreply.github.com>
2023-11-01 17:49:08 -0400

    added a new test Verify the user is able to choose the Freestyle Project from the options list (#96)

--- cypress/e2e/newItemFreestyleProject.cy.js
+++ cypress/e2e/newItemFreestyleProject.cy.js
@@ -19,12 +19,20 @@ describe('New Item > Create Freestyle Project', () => {
 
     });
 
-    it('TC_03.02.004 | New item > Create Freestyle Project > Verify the user is able to see an empty field warning after choosing the "Freestyle Project" from the option list', () => {
+    it('TC_03.02.004 | Verify the user is able to see an empty field warning after choosing the "Freestyle Project" from the option list', () => {
 
         cy.get('form div#itemname-required').as('emptyFieldWarning').should('have.class', 'input-message-disabled').and('not.be.visible');
         cy.get('form li[class$=FreeStyleProject]').click();
 
         cy.get('@emptyFieldWarning').should('not.have.class', 'input-message-disabled').and('have.text', newItemFreestyleProject.emptyFieldWarning).and('be.visible');
+
+    });
+
+    it('TC_03.02.005 | Verify the user is able to choose the "Freestyle Project" from the options list', () => {
+
+        cy.get('form li[class*=FreeStyleProject]').as('freestyleProjectOption').click();
+
+        cy.get('@freestyleProjectOption').should('have.class', 'active').and('have.attr', 'aria-checked', 'true');
 
     });
 });
