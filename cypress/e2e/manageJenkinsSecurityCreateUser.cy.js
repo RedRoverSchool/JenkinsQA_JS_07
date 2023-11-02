f4ddf14ef30bc28575835ab005ce638311cbfdfb
olenaborysonok <144932244+olenaborysonok@users.noreply.github.com>
2023-11-02 15:12:32 -0400

    Added steps for delete user (#110)

--- cypress/e2e/manageJenkinsSecurityCreateUser.cy.js
+++ cypress/e2e/manageJenkinsSecurityCreateUser.cy.js
@@ -35,7 +35,20 @@ describe('ManageJenkinsSecurityCreateUser.cy', () => {
         });
       });
 
-    cy.get('a[data-url="user/johnkennedy/doDelete"]').pause().click();
+    //***Delete user using dropdown menu by clicking on Confirm Pop-up window click on "Ok"************
+    cy.wait(2000);
+    cy.get('a[href="user/johnkennedy/"] button').click({ force: true });
+    cy.get('button[href="/user/johnkennedy/doDelete"]').click();
+    cy.on('window:confirm', (str) => {
+      expect(str).to.eql('Delete: are you sure?');
+    });
+    cy.get('a[href="/manage"]').click();
+    cy.get('a[href="securityRealm/"]').click();
+    cy.get('a[href="user/johnkennedy/"] button').should('not.exist');
+
+    //***Delete user using delete button**************
+    //cy.wait(2000);
+    //cy.get('a[data-url="user/johnkennedy/doDelete"]').click();
   });
 
   it('Manage Jenkins > Security > Create user > Verify error message displayed when creating user without username', () => {
@@ -46,11 +59,19 @@ describe('ManageJenkinsSecurityCreateUser.cy', () => {
     cy.get('a[href="addUser"]').click();
     cy.title().should('include', 'Create User');
     cy.get('#username').should('not.have.value');
-    cy.get('input[name="password1"]').type(manageJenkinsSecurityCreateUser.password);
-    cy.get('input[name="password2"]').type(manageJenkinsSecurityCreateUser.password);
-    cy.get('input[name="fullname"]').type(manageJenkinsSecurityCreateUser.fullName);
+    cy.get('input[name="password1"]').type(
+      manageJenkinsSecurityCreateUser.password
+    );
+    cy.get('input[name="password2"]').type(
+      manageJenkinsSecurityCreateUser.password
+    );
+    cy.get('input[name="fullname"]').type(
+      manageJenkinsSecurityCreateUser.fullName
+    );
     cy.get('input[name="email"]').type(manageJenkinsSecurityCreateUser.email);
     cy.get('button[name="Submit"]').click();
-    cy.get('div.error').should('contain', '""').and('contain', manageJenkinsSecurityCreateUser.errorUsername);
+    cy.get('div.error')
+      .should('contain', '""')
+      .and('contain', manageJenkinsSecurityCreateUser.errorUsername);
   });
 });
