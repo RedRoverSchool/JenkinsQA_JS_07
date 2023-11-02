8a1571086211c52e667c65d6cfe77d9a56bd8ff7
KravchukKseniya <39743588+KravchukKseniya@users.noreply.github.com>
2023-11-01 17:17:00 -0700

    TC_03.08.005 | New Item > Copy from > Redirect to configure page (#93)

--- cypress/e2e/newItemCopyFrom.cy.js
+++ cypress/e2e/newItemCopyFrom.cy.js
@@ -33,4 +33,13 @@ describe('New item > Copy from', () => {
         cy.get('#from').type(data.firstProjectName[0]);
         cy.get('.item-copy .yui-ac-content').should('be.visible').and('contain.text', data.firstProjectName);
     });
+
+    it('TC_03.08.005 | +New Item > Copy from > Redirect to configure page', () => {
+        cy.get('a[href="/view/all/newJob"]').click();
+        cy.get('input#name').type(data.secondProjectName);
+        cy.get('.hudson_model_FreeStyleProject').click();
+        cy.get('#from').type(data.firstProjectName);
+        cy.get('#ok-button').click();
+        cy.url().should('be.equal', `http://localhost:8080/job/${data.secondProjectName}/configure`)
+    });
 });
