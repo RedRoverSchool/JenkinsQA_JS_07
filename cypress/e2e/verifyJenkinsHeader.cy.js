cff15d49a4e3c8a1716f7078f5d606813469f9fb
NatalieDiekmann <nataliia.diekmann@gmail.com>
2023-11-02 09:22:40 -0500

    'Changed exist - deleted be'

--- cypress/e2e/verifyJenkinsHeader.cy.js
+++ cypress/e2e/verifyJenkinsHeader.cy.js
@@ -3,11 +3,11 @@
 describe('verifyHeaderLogoClickable', () => {
     it('TC_02.02.002 | Header > Verify Jenkins Logo (image) and Jenkins Icon (image with text Jenkins) are visible, clickable and after click redirect to Homepage', () => {
        
-        cy.get('#jenkins-head-icon').should('be.exist').and('be.visible').click()
+        cy.get('#jenkins-head-icon').should('exist').and('be.visible').click()
         cy.url().should('eq', 'http://localhost:8080/')
-        cy.get('#jenkins-name-icon').should('be.exist').and('be.visible').click()
+        cy.get('#jenkins-name-icon').should('exist').and('be.visible').click()
         cy.url().should('eq', 'http://localhost:8080/')
-        cy.get('a[id="jenkins-home-link"]').should('be.exist').and('be.visible').click()
+        cy.get('a[id="jenkins-home-link"]').should('exist').and('be.visible').click()
         cy.url().should('eq', 'http://localhost:8080/')
        
     });
