a336aed55bf37ccbac0496b19ad889db89bf856c
ElenaDetusheva <e.det@list.ru>
2023-11-02 09:06:42 +0300

    added_check_link_People

--- cypress/e2e/dashboardLinkPeaple.cy.js
+++ cypress/e2e/dashboardLinkPeaple.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 
+
 describe('dashboardLinkPeople', () => {
     it(' Dashboard> Check "People" link', () => {
         cy.get('a[href="/asynchPeople/"]').should('have.text', 'People').click();
