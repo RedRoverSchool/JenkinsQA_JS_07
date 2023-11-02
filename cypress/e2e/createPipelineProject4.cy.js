52027dc38fa2093839adfd398900e206f478662b
Albina-star <akhalim1@student.ccp.edu>
2023-11-02 18:16:09 -0400

    Check input-validation-message

--- cypress/e2e/createPipelineProject4.cy.js
+++ cypress/e2e/createPipelineProject4.cy.js
@@ -18,4 +18,12 @@ describe('createPipelineProject', () => {
       cy.get('td:nth-child(3) > a > span').should('have.text', this.allNames.newProjectName)
 
   })
+
+  it('TC_03.04.009 | Check input-validation-message', function () {
+    cy.get("a[href='/view/all/newJob']").click();
+    cy.get('li.org_jenkinsci_plugins_workflow_job_WorkflowJob > div.icon').click();
+
+    cy.get("div[class='input-validation-message']").should('be.visible');
+    cy.get('#ok-button').should('be.disabled');
+  })
 })
