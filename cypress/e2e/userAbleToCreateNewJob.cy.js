92192cfd7f8baf8b83857f43621c052d43692022
gurinowicz <tkachukya@gmail.com>
2023-11-03 15:36:45 +0300

    TC_03.08.008 | User is able to create a New job

---
+++ cypress/e2e/userAbleToCreateNewJob.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress"/>
+
+describe('user_able_to_create_new_job', () => {
+       it('TC_03.08.008 +New Item > Copy from > User is able to create a New job', () => {
+        cy.get('a[href="/view/all/newJob"]').should('exist').click()
+        cy.get('a[href="/view/all/newJob"]').should('not.exist')
+        cy.url().should('be.equal', 'http://localhost:8080/view/all/newJob')
+       });
+});
