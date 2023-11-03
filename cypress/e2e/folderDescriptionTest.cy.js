d729d850ecd65576acafdfd501c3898eef1dc73e
Anna Kuzmina <annkzmn@gmail.com>
2023-11-02 18:04:09 -0600

    add local host change

---
+++ cypress/e2e/folderDescriptionTest.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+describe('folder description test TC_07.02.001', () => {
+
+    it('folder description test', function () {
+        cy.visit('http://localhost:8080/view/all/newJob')
+        cy.get('#name').type('my_folder')
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
+        cy.get('#ok-button').click()
+        cy.get('a[href="/job/my_folder/"]').click()
+        cy.get('#description').should('be.empty')
+        cy.get('#description-link').should('be.visible').and('have.text', 'Add description')
+
+    })
+
+})
