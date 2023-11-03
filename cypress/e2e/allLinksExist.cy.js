bca37fc8db86edd9049527d8206eabea4cda233f
Elena Pakhomova <140397499+pakhomova-ev@users.noreply.github.com>
2023-11-04 00:02:03 +0300

    Delete cypress/e2e/allLinksExist.cy.js

--- cypress/e2e/allLinksExist.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress"/>
-describe('allLinks_Exist', () => {
-    it('allLinksExist> All links exist in the right order', () => {
-     cy.get('#tasks').should(($) => {
-        expect($.get(0).innerText)
-        .to.eq('New Item\nPeople\nBuild History\nManage Jenkins\nMy Views')
-      })
-        });
-});
