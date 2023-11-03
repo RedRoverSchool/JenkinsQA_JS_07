a15a53b8f1a7c9fc487c41adcc26d2ba63a98150
NadyaShulga <shulgaqa@gmail.com>
2023-11-02 20:09:01 -0400

    change methods

--- cypress/e2e/iconJenkinsTest.cy.js
+++ cypress/e2e/iconJenkinsTest.cy.js
@@ -3,8 +3,8 @@
 describe('iconJenkinsTest', () => {
 
     it('TC_02.02.001 | Verify icon and logo on the first page', function () {
-        cy.get('#jenkins-head-icon').click().should('be.visible');
-        cy.get('#jenkins-name-icon').click().should('be.visible');
+        cy.get('#jenkins-head-icon').should('be.visible').click();
+        cy.get('#jenkins-name-icon').should('be.visible').click();
 
     });
 })
