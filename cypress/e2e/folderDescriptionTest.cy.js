a560847ba4f837d2bbecf21ca81cc50f11e9975f
Anna Kuzmina <annkzmn@gmail.com>
2023-11-02 21:40:11 -0600

    add final version of the code

--- cypress/e2e/folderDescriptionTest.cy.js
+++ cypress/e2e/folderDescriptionTest.cy.js
@@ -8,8 +8,10 @@ describe('folder description test TC_07.02.001', () => {
         cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
         cy.get('#ok-button').click()
         cy.get('a[href="/job/my_folder/"]').click()
-        cy.get('#description').should('be.empty')
-        cy.get('#description-link').should('be.visible').and('have.text', 'Add description')
+        
+        cy.get('#description').should('have.value', '')
+        cy.get('#description-link').should('be.visible')
+        cy.contains('#description-link', 'Add description')
 
     })
 
