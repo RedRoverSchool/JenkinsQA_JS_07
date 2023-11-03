89700dae0d6c6edef64d89e23a9fd7ba87c0f6d7
gurinowicz <tkachukya@gmail.com>
2023-11-03 11:03:19 +0300

    fixed test

--- cypress/e2e/redirectionNewProjectPage.cy.js
+++ cypress/e2e/redirectionNewProjectPage.cy.js
@@ -1,13 +1,12 @@
 /// <reference types="cypress"/>
 
-import newJobPage from "../fixtures/newJobPage.json"
-import newItem from "../fixtures/newItem.json"
-
 describe('redirectionNewProjectPage', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8080/view/all/newJob')
+    });
     it('TC_03.08.007 +New Item > Copy from > Redirect to the new project page', () => {
-        cy.visit('http://localhost:8080/view/all/newJob');
         cy.get('input#name').type('NewProject');
-        cy.get('.hudson_model_FreeStyleProject').click();
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click();
         cy.get('#ok-button').click();
         cy.url().should('be.equal', 'http://localhost:8080/job/NewProject/configure')
     });
