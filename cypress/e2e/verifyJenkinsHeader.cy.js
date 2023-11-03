9bbcaac9e2679de0ada9a350059f90d23d65ca5f
Tema <shvetsartem7@gmail.com>
2023-11-02 23:31:32 -0400

    'TC_02.02.006 | Header > Jenkins Logo'

--- cypress/e2e/verifyJenkinsHeader.cy.js
+++ cypress/e2e/verifyJenkinsHeader.cy.js
@@ -11,4 +11,9 @@ describe('verifyHeaderLogoClickable', () => {
         cy.url().should('eq', 'http://localhost:8080/')
        
     });
+
+    it('TC_02.02.006 | Header > Jenkins Logo is visible and after click redirect to Homepage', () => {
+        cy.get('#jenkins-head-icon').should('be.visible').click();
+        cy.get('.jenkins-breadcrumbs').should('have.text',"Dashboard").and('have.length',1);
+    })
 });
