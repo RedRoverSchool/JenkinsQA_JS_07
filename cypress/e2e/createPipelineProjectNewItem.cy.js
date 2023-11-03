611e1d3ca8837beed1d7277ad21f89bbbca1da6d
Elena Pakhomova <pakhomova.e@gmail.com>
2023-11-03 18:34:15 +0300

    RF | TC_03.04.008 | Create Pipeline project > Not unique name > Message invalid item name

--- cypress/e2e/createPipelineProjectNewItem.cy.js
+++ cypress/e2e/createPipelineProjectNewItem.cy.js
@@ -4,7 +4,7 @@ let nameProject= "firstPipelineProject"
 
 describe('createPipelineProjectNewItem', () => {
 
-    it('TC_03.04.005 | Create Pipeline project > Not unique name > Inactive button', () => {
+    it('TC_03.04.008 | Create Pipeline project > Not unique name > Message invalid item name', () => {
         cy.visit('http://localhost:8080/')
         cy.get("a[href='/view/all/newJob']").click()
         cy.get('input#name').type(nameProject)
@@ -13,8 +13,6 @@ describe('createPipelineProjectNewItem', () => {
         cy.get('a[href="/"].model-link').click()
         cy.get("a[href='/view/all/newJob']").click()
         cy.get('input#name').type(nameProject)
-        cy.get('li[tabindex="0"] span').contains('Pipeline').click()
-
-        cy.get('#ok-button').should('be.disabled')
+        cy.get('div#itemname-invalid.input-validation-message').should('be.visible').and('have.text',`» A job already exists with the name ‘${nameProject}’`)
     })
-    })
+})
