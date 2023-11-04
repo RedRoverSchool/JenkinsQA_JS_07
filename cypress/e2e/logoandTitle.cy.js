ab1ca70d4de55b3e813ad3950d9a156a2d3af75d
Oxana Reshes <oxanareshes@gmail.com>
2023-11-04 14:50:28 -0700

    TC_02.02.01 |Verify Jenkins Logo and Title

--- cypress/e2e/logoandTitle.cy.js
+++ cypress/e2e/logoandTitle.cy.js
@@ -1,13 +1,17 @@
 /// <reference types="cypress"/>
-
-describe('Logo and Title', () => {
+import {JenkinsHome} from '../fixtures/logoAndTitle.json'
+describe ('Logo and Title', () => {
+let allNamesNew;
+// {
+//   const JenkinsHome = {
+//     "welcomePage":"Welcome to Jenkins!"
+//   }
 
   it('TC_02.02.01 |Verify Jenkins Logo and Title', function () {
     cy.get('#jenkins-home-link').should('be.visible')
 
     cy.get('a[href="/view/all/newJob"]').click()
     cy.get('#jenkins-home-link').click()
-    cy.get('.empty-state-block h1').should('have.text', 'Welcome to Jenkins!').and('be.visible')
+    cy.get('.empty-state-block h1').should('have.text', JenkinsHome).and('be.visible')
   })
 })
-
