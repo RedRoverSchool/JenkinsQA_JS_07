1b23fab7946d7d1202466c717a4e442df6a7ba2b
YuriySEM <yuko40118@gmail.com>
2023-11-04 14:06:28 -0500

    TC_03.03.004 Verify  Freestyle Project is created

--- cypress/e2e/createFreestyleProject.cy.js
+++ cypress/e2e/createFreestyleProject.cy.js
@@ -24,4 +24,11 @@ describe('createFreestyleProject', () => {
 
         cy.get('textarea[name="description"]').should('exist').and('be.visible');
     });
+
+    it('TC_03.03.004 | Verify that Freestyle Project is created and verify elements on /configure page', function () {
+        cy.get('[href*="/view/all/newJob"]').click();
+        cy.get('input#name').type('project' + Date.now());
+        cy.get('li.hudson_model_FreeStyleProject').click();
+    });
+
 });
