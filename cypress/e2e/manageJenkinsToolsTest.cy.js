9e43951054957923101b1cfe6326b29684dccfc9
1irinabut <1irinabut@gmail.com>
2023-11-04 17:40:28 +0700

    added manageJenkinsToolsTest

---
+++ cypress/e2e/manageJenkinsToolsTest.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('manageJenkinsToolsTest', () => {
+  it('TC_09.06.008 | Go to the "Tools" page', () => {
+    cy.get('a[href="/manage"]').click();
+    cy.location('pathname').should('eq', '/manage/');
+    cy.get('a[href="configureTools"]').click();
+    cy.get(
+      '#breadcrumbs .jenkins-breadcrumbs__list-item:nth-last-of-type(2)'
+    ).should('have.text', 'Tools');
+  });
+});
