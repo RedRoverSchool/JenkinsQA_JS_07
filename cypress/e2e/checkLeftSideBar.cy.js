a67c3219326bf1d18d1c4db1ffba69556d1c4ab5
JackBlaaack <eugene.lapytko@gmail.com>
2023-11-05 22:14:01 +0300

    deleted string console.log(elementText)

--- cypress/e2e/checkLeftSideBar.cy.js
+++ cypress/e2e/checkLeftSideBar.cy.js
@@ -6,7 +6,6 @@ describe("Check left side bar", () => {
   it("TC_02.04.018 | Dashboard > Verify Command panel on the left side", () => {
     cy.get("#tasks").then(($element) => {
       const elementText = $element.text().replace(/\n/g, '').trim().split(/\s{2,}/);
-      console.log(elementText);
       expect(elementText).to.deep.equal(namesData);
 
     });
