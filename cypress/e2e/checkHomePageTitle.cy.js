00557b0532c390710dff4f8205c35f4b21bdb710
Palina Liasovich <palina.liasovich@gmail.com>
2023-11-05 20:27:51 +0200

    RF my test

--- cypress/e2e/checkHomePageTitle.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress"/>
-
-describe('displaying Title', () =>{
-    it('TC_02.01.002 | Dashboard > Home page>displaying Title'), () =>{
-        cy.get('#jenkins-name-icon').should('be.visible').and('include.text', "Jenkins")
-    }
-})
-  
