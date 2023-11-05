9fa790cd6f844164065510ed3b1b062f0933abec
Nataly-Mayatskaya <130170775+Nataly-Mayatskaya@users.noreply.github.com>
2023-11-05 19:44:41 +0200

    RF_redirectToToolsPage (#155)

--- cypress/e2e/redirectToToolsPage.cy.js
+++ cypress/e2e/redirectToToolsPage.cy.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress"/>
 
-describe('redirectToToolsPage', () => {
-    it('TC_09.06.006|Redirect to Tools page', () => {
-        cy.visit('http://localhost:8080/manage/')
-        cy.get(':nth-child(5) > .jenkins-section__items > :nth-child(2) > a').should('be.visible').click()
+describe('redirectToToolsPage',() => {
+    it('TC_09.06.006|Redirect to Tools page',() => {
+        cy.get('a[href="/manage"]').click()
         
-        cy.url().should('be.equal', 'http://localhost:8080/manage/configureTools/')
-        cy.get('h1').should('exist')
+        cy.get('a[href="configureTools"]').click()
+        cy.url().should('include','/manage/configureTools/')
+        cy.get('h1').should('be.visible').and('have.text','Tools')
     });
 });
