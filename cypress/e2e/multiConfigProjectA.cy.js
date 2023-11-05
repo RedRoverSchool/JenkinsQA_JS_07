7552618db2afcdfe6f0a996575ad8d11e3bfaa0f
Rudenko-Anna <rudenko_anna_08@ukr.net>
2023-11-05 12:02:37 +0200

    add fixtures

--- cypress/e2e/multiConfigProjectA.cy.js
+++ cypress/e2e/multiConfigProjectA.cy.js
@@ -1,14 +1,14 @@
 /// <reference types="cypress"/>
-
+import multiConfigProjectA from "../fixtures/multiConfigProjectA.json"
 describe('multiConfigProjectA', () => {
 
     it('Create Multiconfiguration project', () => {
         cy.get('a[href="/view/all/newJob"]').click()
-        cy.get('input#name').type('Project073')
+        cy.get('input#name').type(multiConfigProjectA.ProjectName)
         cy.get('li[tabindex="0"] span').contains('Multi-configuration project').click()
         cy.get('#ok-button').click()
         cy.get('.jenkins-breadcrumbs__list-item:nth-child(1)').click()
 
-        cy.get('td a[href*="job"].jenkins-table__link').should('have.text', 'Project073')
+        cy.get('td a[href*="job"].jenkins-table__link').should('have.text', multiConfigProjectA.ProjectName)
     });
 })
