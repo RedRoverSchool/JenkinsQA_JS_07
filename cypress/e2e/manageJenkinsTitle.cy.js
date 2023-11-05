e5b35e0827f669312346f368b274f5ccda436c36
RuslanG13 <ruslan.gudenko13@ukr.net>
2023-11-05 08:31:35 +0200

    Small fix name of fixture variable

--- cypress/e2e/manageJenkinsTitle.cy.js
+++ cypress/e2e/manageJenkinsTitle.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress"/>
 
-import manageJenkinsTitle from "../fixtures/manageJenkinsTitle.json";
+import manageJenkinsTitleData from "../fixtures/manageJenkinsTitle.json";
 
 describe("manageJenkinsTitle", () => {
     beforeEach(() => {
@@ -8,14 +8,14 @@ describe("manageJenkinsTitle", () => {
     })
 
     it("TC_09.01.001 | Manage Jenkins > Title > Main header displays verification", () => {
-        cy.url().should("include", manageJenkinsTitle.manageJenkinsEndpoint);
+        cy.url().should("include", manageJenkinsTitleData.manageJenkinsEndpoint);
 
         cy.get(".jenkins-app-bar__content > h1").should("be.visible")
-            .and("have.text", manageJenkinsTitle.mainHeaderPage);
+            .and("have.text", manageJenkinsTitleData.mainHeaderPage);
     });
 
     it("TC_09.01.002 | Manage Jenkins > Title > 5 section-headers displays verification", () => {
-        const objOfSubHeadersFromJSON = JSON.parse(JSON.stringify(manageJenkinsTitle.subHeadersPage));
+        const objOfSubHeadersFromJSON = JSON.parse(JSON.stringify(manageJenkinsTitleData.subHeadersPage));
         const arrOfSubHeadersFromObj = Object.values(objOfSubHeadersFromJSON);
 
         cy.get(".jenkins-section > h2").should("be.visible")
