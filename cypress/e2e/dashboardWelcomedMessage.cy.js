f243216b3f983ac24eee1638fc5fc37850e2731a
Pavel Korz <131908496+Paul-Korz@users.noreply.github.com>
2023-11-05 17:27:58 +0300

    RF | dashboardWelcomedMessage | TC_02.03.006 | Verify th Welcome Message (#152)

--- cypress/e2e/dashboardWelcomedMessage.cy.js
+++ cypress/e2e/dashboardWelcomedMessage.cy.js
@@ -1,7 +1,9 @@
 /// <reference types="cypress"/>
 
+import welcomedMessage from "../fixtures/dashboardWelcomedMessage.json"
+
 describe('dashboardWelcomedMessage', () => {
     it('TC_02.03.006 | Verify the Welcome Message', () => {
-      cy.get('div.empty-state-block > h1').should('have.text', 'Welcome to Jenkins!')
+      cy.get('div.empty-state-block > h1').should('be.visible').and('have.text', welcomedMessage.pageTitle)
     });
 });
