81263d0cde67dc97f0a3ab70d448ddc119c32213
MargaritaArcady <alquavein@gmail.com>
2023-11-04 20:01:19 -0400

    changed elements

--- cypress/e2e/multiconfigProject.cy.js
+++ cypress/e2e/multiconfigProject.cy.js
@@ -3,12 +3,13 @@
 describe('multiconfigProject', () => {
 
     it('Create Multiconfiguration project', function () {
-        cy.get('a[href="newJob]').click()
+        cy.get('a[href="newJob"]').click()
         cy.get('input#name').type('Project1')
         cy.get('li[tabindex="0"] span').contains('Multi-configuration project').click()
         cy.get('#ok-button').click()
-        cy.get('.jenkins-breadcrumbs_list-item:nth-child(1) > .model-link').click()
-        
-        cy.get('td a[href*="job"].jenkins-table_link').should('have.text', 'Projtct1')
+        cy.get('.jenkins-breadcrumbs__list-item:nth-child(1) > .model-link').click()
+
+
+        cy.get('td a[href*="job/Project1"].jenkins-table__link').should('have.text', 'Project1')
     }) 
 })
