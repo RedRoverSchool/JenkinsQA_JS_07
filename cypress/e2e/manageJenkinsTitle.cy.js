c7ade089fc02e930a22ede0dcc584b62e04337d9
RuslanG13 <ruslan.gudenko13@ukr.net>
2023-11-05 08:19:26 +0200

    fixed after review comments

--- cypress/e2e/manageJenkinsTitle.cy.js
+++ cypress/e2e/manageJenkinsTitle.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress"/>
 
-import { manageJenkinsEndpoint, mainHeaderPage } from "../fixtures/manageJenkinsTitle.json";
+import manageJenkinsTitle from "../fixtures/manageJenkinsTitle.json";
 
 describe("manageJenkinsTitle", () => {
     beforeEach(() => {
@@ -8,17 +8,15 @@ describe("manageJenkinsTitle", () => {
     })
 
     it("TC_09.01.001 | Manage Jenkins > Title > Main header displays verification", () => {
-        cy.url().should("include", manageJenkinsEndpoint);
+        cy.url().should("include", manageJenkinsTitle.manageJenkinsEndpoint);
 
         cy.get(".jenkins-app-bar__content > h1").should("be.visible")
-            .and("have.text", mainHeaderPage);
+            .and("have.text", manageJenkinsTitle.mainHeaderPage);
     });
 
-    it.skip("TC_09.01.002 | Manage Jenkins > Title > 5 section-headers displays verification", () => {
+    it("TC_09.01.002 | Manage Jenkins > Title > 5 section-headers displays verification", () => {
         const objOfSubHeadersFromJSON = JSON.parse(JSON.stringify(manageJenkinsTitle.subHeadersPage));
         const arrOfSubHeadersFromObj = Object.values(objOfSubHeadersFromJSON);
-
-        cy.get(".task-link-wrapper > a[href='/manage']").click();
 
         cy.get(".jenkins-section > h2").should("be.visible")
             .and("have.prop", "tagName", "H2")
