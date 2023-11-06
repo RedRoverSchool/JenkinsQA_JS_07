9e08cc617f7aef4774f29b1655ebfc42889c8d73
Albina-star <akhalim1@student.ccp.edu>
2023-11-06 17:29:28 -0500

    elements of Configure menu

--- cypress/e2e/pipelineProject.cy.js
+++ cypress/e2e/pipelineProject.cy.js
@@ -4,19 +4,21 @@ import pipelineProject from '../fixtures/pipelineProject.json'
 
 describe('New Item > Create Pipeline Project > Configure new Pipeline project', () => {
 
-    it('TC_03.05.004 | New Item > Create Pipeline Project > Configure new Pipeline project > Verify quantity of elements of Configure menu', function () {
+    it('TC_03.05.004 | New Item > Create Pipeline Project > Configure new Pipeline project >Verify quantity of elements of Configure menu', function () {
        cy.get("a[href='/view/all/newJob']").click();
        cy.get("input[class='jenkins-input']").type(pipelineProject.pipelineName);
        cy.get('li.org_jenkinsci_plugins_workflow_job_WorkflowJob > div.icon').click();
        cy.get('#ok-button').click();
 
-       cy.url().should('be.equal', 'http://localhost:8080/job/FirstPipelineProject/configure');
-       cy.get('h1').should('have.text', 'Configure');
+       cy.url().should('include', '/configure')
+       cy.get('h1').should('have.text', pipelineProject.pipelineConfigurePageHeader);
 
-       cy.get("div #tasks [class='task']")
+       cy.get("div.task")
+       .should("have.length", pipelineProject.configureMenuLength)
        .then(($els) => {
-        return Cypress.$.makeArray($els);
-    })
-      .should("have.length", "3")
+          return Cypress.$.makeArray($els)
+          .map(($el) => $el.innerText)
+       })
+      .should('deep.eql', pipelineProject.expectedConfigureMenuItems)
     });
 });
