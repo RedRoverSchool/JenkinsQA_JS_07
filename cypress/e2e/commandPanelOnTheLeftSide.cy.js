be6548e79d9d9298edd65b08bc66e84512613a48
JackBlaaack <eugene.lapytko@gmail.com>
2023-11-06 15:24:51 +0300

    changed name of it

--- cypress/e2e/commandPanelOnTheLeftSide.cy.js
+++ cypress/e2e/commandPanelOnTheLeftSide.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 
 describe('Command panel on the left side', ()=>{
-    it('TC_02.04.019 | Item > Check icons near the link', ()=>{
+    it('TC_02.04.019 |Dashboard > Command panel on the left side> Check icons near the link', ()=>{
         cy.get('a.task-link').each(($link)=>{
             const $parent = $link.parent();
             const hasIcon = $parent.find('.task-icon-link').length > 0;
