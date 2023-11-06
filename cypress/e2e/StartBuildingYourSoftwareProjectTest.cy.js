0a982fe5d1d29800ceb622e2541505dab34125d0
JackBlaaack <eugene.lapytko@gmail.com>
2023-11-06 19:10:53 +0300

    added it TC_02.06.004

--- cypress/e2e/StartBuildingYourSoftwareProjectTest.cy.js
+++ cypress/e2e/StartBuildingYourSoftwareProjectTest.cy.js
@@ -5,4 +5,9 @@ describe('StartBuildingYourSoftwareProjectTest', () =>{
     it('TC_02.06.001 |Block “Start building your software project” > Verify "Start building your software project” is displayed', ()=>{
 cy.get('h2.h4').should('be.visible');
     });
+    it('TC_02.06.004 |Block “Start building your software project” >Check redirect to the page "New Job"', ()=>{
+
+        cy.get('a[href="newJob"]').click();
+        cy.url().should('be.eql', 'http://localhost:8080/newJob');
+        })
 });
