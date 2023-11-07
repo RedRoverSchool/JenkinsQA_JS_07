bf78d844f4a5ad2edb3db3f91e0ee64f1d1e5151
JackBlaaack <eugene.lapytko@gmail.com>
2023-11-07 23:30:37 +0300

    added variable PORT

--- cypress/e2e/StartBuildingYourSoftwareProjectTest.cy.js
+++ cypress/e2e/StartBuildingYourSoftwareProjectTest.cy.js
@@ -1,13 +1,15 @@
 /// <reference types="cypress"/>
 
-describe('StartBuildingYourSoftwareProjectTest', () =>{
+const PORT = Cypress.env('local.port');
 
+describe('StartBuildingYourSoftwareProjectTest', () =>{
+ 
     it('TC_02.06.001 |Block “Start building your software project” > Verify "Start building your software project” is displayed', ()=>{
 cy.get('h2.h4').should('be.visible');
     });
     it('TC_02.06.004 |Block “Start building your software project” >Check redirect to the page "New Job"', ()=>{
 
         cy.get('a[href="newJob"]').click();
-        cy.url().should('be.eql', 'http://localhost:8080/newJob');
-        })
+        cy.url().should('be.eql',`http://localhost:${PORT}/newJob`);
+        });
 });
