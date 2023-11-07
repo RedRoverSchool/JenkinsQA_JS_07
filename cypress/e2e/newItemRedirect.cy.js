1de8cf3fd904e3c0e9844af8ef95b35309bbc31e
ArtemDe <72464734+derearte@users.noreply.github.com>
2023-11-07 00:16:35 -0800

    TC_03.01.010 | + New Item > Verify that the following types of projects: “Freestyle project”, “Pipeline”, “Multi-configuration project”, “Folder”, “Multibranch Pipeline”, “Organization Folder “ are displayed. (#172)

--- cypress/e2e/newItemRedirect.cy.js
+++ cypress/e2e/newItemRedirect.cy.js
@@ -2,12 +2,21 @@
 
 import newJobPage from "../fixtures/newJobPage.json"
 
-describe('newItemRedirect', () => {
+describe('newItem', () => {
 
     it('TC_03.01.004 | Item > Verify that clicking "+ New Item" redirects the user to the "New Job" page', function () {
         
         cy.get('a[href*="/view/all/newJob"]').click();
 
         cy.get('label[for="name"]').should('have.text', newJobPage.fieldName);
+    });
+
+    it('TC_03.01.010 | + New Item > Verify that the following types of projects: “Freestyle project”, “Pipeline”, “Multi-configuration project”, “Folder”, “Multibranch Pipeline”, “Organization Folder “ are displayed.', () => {
+        
+        cy.get('a[href*="/view/all/newJob"]').click();
+        
+        cy.get('.label').each(($element, index) => {
+            cy.wrap($element).should('have.text', newJobPage.expectedProjectLabels[index]).and('be.visible');
+        });
     });
 });
