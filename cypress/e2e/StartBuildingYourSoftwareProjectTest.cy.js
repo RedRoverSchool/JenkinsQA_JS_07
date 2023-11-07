e83360c5092f128bd5806085fa5866e4405470e7
Nataly-Mayatskaya <130170775+Nataly-Mayatskaya@users.noreply.github.com>
2023-11-07 12:14:42 +0200

    TC_02.06.003 | Block "Start building your software project" > Option “Create a job” leads to the page "New Job" (#173)
    * nm/startBuildingYourSoftwareProjectTest
    
    * make filename lowercase

--- cypress/e2e/StartBuildingYourSoftwareProjectTest.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress"/>
-
-describe('StartBuildingYourSoftwareProjectTest', () =>{
-
-    it('TC_02.06.001 |Block “Start building your software project” > Verify "Start building your software project” is displayed', ()=>{
-cy.get('h2.h4').should('be.visible');
-    });
-});
