faee6a86fdd0c0c9325179c72106ca7215a7d9f4
Oxana Reshes <oxanareshes@gmail.com>
2023-11-06 18:59:10 -0800

    dbCommandPanel

--- cypress/e2e/dbCommandPanel.cy.js
+++ cypress/e2e/dbCommandPanel.cy.js
@@ -1,21 +1,23 @@
 /// <reference types="cypress"/>
+import {links} from "../dbCommandPanel.json"
 
 
 describe('Verify Dashboard links redirected to the corresponding page', () => {
     it('TC_02.04.06 | Dashboard > Command panel on the left side', ()=>{ 
         cy.get('a[href="/view/all/newJob"]').click()
-        cy.url().should('eq', 'http://localhost:8080/view/all/newJob');
+        cy.url().should('eq', links.NewItem);
         cy.get('#jenkins-name-icon').click()
+        
         cy.get('a[href="/asynchPeople/"]').click();
-        cy.url().should('eq', 'http://localhost:8080/asynchPeople/');
-        cy.get('#jenkins-name-icon').click();
+        cy.url().should('eq', links.People);
+        
         cy.get('a[href="/view/all/builds"').click()
-        cy.url().should('eq', 'http://localhost:8080/view/all/builds');
-        cy.get('#jenkins-name-icon').click();
+        cy.url().should('eq', links.BuildHistory);
+        
         cy.get('a[href="/manage"').click()
-        cy.url().should('eq', 'http://localhost:8080/manage/');
-        cy.get('#jenkins-name-icon').click();
+        cy.url().should('eq', links.ManageJenkins);
+        
         cy.get('a[href="/me/my-views"]').click()
-        cy.url().should('eq', 'http://localhost:8080/me/my-views/view/all/')
+        cy.url().should('eq', links.MyViews)
     })
 })
