2d3727b24abb54cef1dd4a61a1810250ee6274ea
Vladimir Vakulenko <vakulenko.volodia@yandex.ru>
2023-11-07 09:27:43 +0200

    07.11.2023 - added test Create Multiconfiguration project

--- cypress/e2e/mcProject.cy.js
+++ cypress/e2e/mcProject.cy.js
@@ -2,15 +2,33 @@
 
 describe('mcProject', () => {
 
-    it('Create Multiconfiguration project', function() {
+    beforeEach(function() {
+        cy.fixture('projectData').then((data) => {
+            this.projectInfo = data.projectData;
+        })
+    })
+
+//     it('Create Multiconfiguration project', function() {
+//         cy.get('a[href="newJob"]').click()
+//         cy.get('.jenkins-input').type('Project 1')
+//         cy.get('li.hudson_matrix_MatrixProject').click()
+//         cy.get('#ok-button').click()
+//         cy.get('.jenkins-button--primary').click()
+//         cy.get('.matrix-project-headline').should('be.visible')
+//         cy.get('#jenkins-home-link').click()
+        
+//         cy.get('.jenkins-table__link').should('have.text', 'Project 1')
+//    })
+
+   it('Create Multiconfiguration project', function() {
         cy.get('a[href="newJob"]').click()
-        cy.get('.jenkins-input').type('Project 1')
+        cy.get('.jenkins-input').type(this.projectInfo.projectName)
         cy.get('li.hudson_matrix_MatrixProject').click()
         cy.get('#ok-button').click()
         cy.get('.jenkins-button--primary').click()
-        cy.get('.matrix-project-headline').should('be.visible', 'Project Project1')
-        cy.visit('http://localhost:8080/')
-
-        cy.get('.jenkins-table__link').should('have.text', 'Project 1')
+        cy.get('.matrix-project-headline').should('be.visible')
+        cy.get('#jenkins-home-link').click()
+        
+        cy.get('.jenkins-table__link').should('have.text', this.projectInfo.projectName)
    })
 })
