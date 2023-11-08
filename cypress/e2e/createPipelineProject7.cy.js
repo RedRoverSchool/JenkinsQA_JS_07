edf41e69370bf82693f5c13bb3bfea31d8cecb05
NadyaSenyukova <nadyasenv86@gmail.com>
2023-11-08 20:58:29 +0300

    added fixtures

--- cypress/e2e/createPipelineProject7.cy.js
+++ cypress/e2e/createPipelineProject7.cy.js
@@ -1,11 +1,18 @@
 /// <reference types="cypress"/>
 
+import pipelineProject from "../fixtures/pipelineProject.json";
+
 describe("createPipelineProject7", () => {
   it("TC_03.04.007 | Create Pipeline project", () => {
     cy.get('a[href="/view/all/newJob"]').click();
-    cy.get("#name").type("Project7");
+    cy.get("#name").type(pipelineProject.pipelineName);
     cy.get(".label").contains("Pipeline").click();
     cy.get("#ok-button").click();
-    cy.url().should("eq", "http://localhost:8080/job/Project7/configure");
+
+    cy.url().should("include", "/configure");
+    cy.get("div h1").should(
+      "have.text",
+      pipelineProject.pipelineConfigurePageHeader
+    );
   });
 });
