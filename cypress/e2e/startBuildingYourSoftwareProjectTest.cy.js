1b608f93083673e763d6c827236a73e1c0e6bc19
Alexey <bainoff@gmail.com>
2023-11-08 19:35:04 +0100

    commit potential conflict

--- cypress/e2e/startBuildingYourSoftwareProjectTest.cy.js
+++ cypress/e2e/startBuildingYourSoftwareProjectTest.cy.js
@@ -1,23 +1,29 @@
 /// <reference types="cypress"/>
-import {expectedProjectLabels}from '../fixtures/newJobPage.json'
-describe('startBuildingYourSoftwareProjectTest',() => {
-    it('TC_02.06.001 |Block “Start building your software project” > Verify "Start building your software project” is displayed',() => {
-        cy.get('h2.h4').should('be.visible');
-    });
 
-    it('TC_02.06.003 | Block "Start building your software project" > Option “Create a job” leads to the page “New Job”',() => {
-        cy.get('a[href="newJob"]').click()
-        cy.url().should('include','/newJob')
-        cy.get('.h3').should('be.visible').and('have.text','Enter an item name')
-    });
+import { expectedProjectLabels } from "../fixtures/newJobPage.json";
 
-    it('TC_02.06.006 | Block "Start building your software project" > "Arrow" icon is visible and clickable',() => {
-        cy.get('a[href="newJob"] svg').should('be.visible')
-        cy.get('a[href="newJob"] svg').click()
-        cy.url().should('include','/newJob')
-        cy.get('.label').then(($els) => {
-        const ProjectLabels = Cypress._.map(Cypress.$.makeArray($els),'textContent');
-        expect(ProjectLabels).to.be.deep.equal(expectedProjectLabels)
-    })
+describe("startBuildingYourSoftwareProjectTest", () => {
+  it('TC_02.06.001 |Block “Start building your software project” > Verify "Start building your software project” is displayed', () => {
+    cy.get('div.empty-state-block > :nth-child(3) h2').should("be.visible")
+        .and("have.text", "Start building your software project");
+  });
+
+  it('TC_02.06.003 | Block "Start building your software project" > Option “Create a job” leads to the page “New Job”', () => {
+    cy.get('a[href="newJob"]').click();
+    cy.url().should("include", "/newJob");
+    cy.get(".h3").should("be.visible").and("have.text", "Enter an item name");
+  });
+
+  it('TC_02.06.006 | Block "Start building your software project" > "Arrow" icon is visible and clickable', () => {
+    cy.get('a[href="newJob"] svg').should("be.visible");
+    cy.get('a[href="newJob"] svg').click();
+    cy.url().should("include", "/newJob");
+    cy.get(".label").then(($els) => {
+      const ProjectLabels = Cypress._.map(
+        Cypress.$.makeArray($els),
+        "textContent"
+      );
+      expect(ProjectLabels).to.be.deep.equal(expectedProjectLabels);
     });
-})
+  });
+});
