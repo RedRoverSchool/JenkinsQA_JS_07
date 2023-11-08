32e624af6a9ced74a9a0b520b15f60596fefd548
Oxana Reshes <oxanareshes@gmail.com>
2023-11-08 10:50:49 -0800

    CommandPanel1

--- cypress/e2e/dbCommandPanel1.cy.js
+++ cypress/e2e/dbCommandPanel1.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress"/>
 import dbCommandPanel1 from "../fixtures/allTasks.json"
 
-describe('Verify Command panel on the left side is desplayed',function (){
-  
-it('TC_02.04.21 | Dashboard > Command panel on the left side', ()=>{
-   
- cy.get('#div.task').then(($els)=> {
-return Cypress.$.makeArray($els).should('have.length', 5)
-.and('deep.equal', dbCommandPanel1.allTasks )  
-})
+describe('Verify Command panel on the left side is desplayed', function () {
+
+    it('TC_02.04.21 | Dashboard > Command panel on the left side', () => {
+
+        cy.get('#div.task').then(($els) => {
+            return Cypress.$.makeArray($els)
+                .should('have.length', 5)
+                .and('deep.equal', dbCommandPanel1.allTasks)
+        })
     });
 })
