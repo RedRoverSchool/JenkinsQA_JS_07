00e04d1b9214fd92539bb3ffc25b06b8da8a9ba6
ElenaDetusheva <148974974+ElenaDetusheva@users.noreply.github.com>
2023-11-08 15:58:11 +0300

    TC_02.06.006 | Block “Start building your software project” > Check "Arrow" icon. (#180)
    * TC_02.06.06_check_Arrow_icon
    
    * fixed_test_check_arrow
    
    * fixed_02_06_6_without_innerT
    
    * delete_only_and_oldData

--- cypress/e2e/startBuildingYourSoftwareProjectTest.cy.js
+++ cypress/e2e/startBuildingYourSoftwareProjectTest.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress"/>
-
+import {expectedProjectLabels}from '../fixtures/newJobPage.json'
 describe('startBuildingYourSoftwareProjectTest',() => {
     it('TC_02.06.001 |Block “Start building your software project” > Verify "Start building your software project” is displayed',() => {
         cy.get('h2.h4').should('be.visible');
@@ -9,5 +9,15 @@ describe('startBuildingYourSoftwareProjectTest',() => {
         cy.get('a[href="newJob"]').click()
         cy.url().should('include','/newJob')
         cy.get('.h3').should('be.visible').and('have.text','Enter an item name')
+    });
+
+    it('TC_02.06.006 | Block "Start building your software project" > "Arrow" icon is visible and clickable',() => {
+        cy.get('a[href="newJob"] svg').should('be.visible')
+        cy.get('a[href="newJob"] svg').click()
+        cy.url().should('include','/newJob')
+        cy.get('.label').then(($els) => {
+        const ProjectLabels = Cypress._.map(Cypress.$.makeArray($els),'textContent');
+        expect(ProjectLabels).to.be.deep.equal(expectedProjectLabels)
+    })
     });
 })
