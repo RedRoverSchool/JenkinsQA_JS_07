605aa010e5a50c32872ac976b6b1e1655ba27256
IreneGur <ari-24@mail.ru>
2023-11-08 23:43:07 +0300

    TC_03.05.005 | PipelineProject > Check help button

--- cypress/e2e/pipelineProject.cy.js
+++ cypress/e2e/pipelineProject.cy.js
@@ -3,13 +3,14 @@
 import pipelineProject from '../fixtures/pipelineProject.json'
 
 describe('New Item > Create Pipeline Project > Configure new Pipeline project', () => {
+   beforeEach("createPipelineProject", () => {
+      cy.get("a[href='/view/all/newJob']").click();
+      cy.get("input[class='jenkins-input']").type(pipelineProject.pipelineName);
+      cy.get('li.org_jenkinsci_plugins_workflow_job_WorkflowJob > div.icon').click();
+      cy.get('#ok-button').click();
+   });
 
     it('TC_03.05.004 | New Item > Create Pipeline Project > Configure new Pipeline project >Verify quantity of elements of Configure menu', function () {
-       cy.get("a[href='/view/all/newJob']").click();
-       cy.get("input[class='jenkins-input']").type(pipelineProject.pipelineName);
-       cy.get('li.org_jenkinsci_plugins_workflow_job_WorkflowJob > div.icon').click();
-       cy.get('#ok-button').click();
-
        cy.url().should('include', '/configure')
        cy.get('h1').should('have.text', pipelineProject.pipelineConfigurePageHeader);
 
@@ -21,4 +22,18 @@ describe('New Item > Create Pipeline Project > Configure new Pipeline project', 
        })
       .should('deep.eql', pipelineProject.expectedConfigureMenuItems)
     });
+    
+    it('TC_03.05.005 | New Item > Create Pipeline Project > Configure new Pipeline project > Check the help button Discard old builds', () => {
+       cy.get('#cb2')
+         .parents('.jenkins-checkbox-help-wrapper')
+         .find('a')
+         .click()         
+         .parents('.optionalBlock-container.jenkins-form-item')
+         .wait(2000)
+         .find('.help')
+         .should('be.visible')
+         .then($el =>  {
+            expect($el.get(0).innerText).to.eq(pipelineProject.discardOldBuildsHelpText);
+         });
+   });
 });
