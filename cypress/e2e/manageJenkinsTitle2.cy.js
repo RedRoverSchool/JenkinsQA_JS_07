38d8e7c33d26e8092f712629e6e3b1c56e449f36
Nadezhda <abdulragimova.n@gmail.com>
2023-11-09 22:18:35 +0400

    TC_09.01.006 | Manage Jenkins > Title

--- cypress/e2e/manageJenkinsTitle2.cy.js
+++ cypress/e2e/manageJenkinsTitle2.cy.js
@@ -1,9 +1,11 @@
 /// <reference types="cypress"/>
 
+import manageJenkinsTitle from "../fixtures/manageJenkinsTitle.json"
+
 describe('manageJenkinsTitle2', () => {
 
     it('TC_09.01.006 | Manage Jenkins > Title', function() {
         cy.get('.task-link-wrapper  > a[href="/manage"]').click()
-        cy.get('.jenkins-app-bar__content > h1').should('be.visible').and('have.text', 'Manage Jenkins')
+        cy.get('.jenkins-app-bar__content > h1').should('be.visible').and('have.text', manageJenkinsTitle.mainHeaderPage)
     })
 })
