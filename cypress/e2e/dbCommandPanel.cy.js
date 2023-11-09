6dc85d0ebf3c36ae4163a891c5dc5b5c6716c973
Oxana Reshes <oxanareshes@gmail.com>
2023-11-08 18:16:05 -0800

    Dashboard > Command panel on the left side

--- cypress/e2e/dbCommandPanel.cy.js
+++ cypress/e2e/dbCommandPanel.cy.js
@@ -1,23 +1,23 @@
 /// <reference types="cypress"/>
-import {links} from "../fixtures/dbCommandPanel.json"
+ import {links} from "../fixtures/dbCommandPanel.json"
 
 
 describe('Verify Dashboard links redirected to the corresponding page', () => {
     it('TC_02.04.06 | Dashboard > Command panel on the left side', ()=>{ 
         cy.get('a[href="/view/all/newJob"]').click()
-        cy.url().should('eq', links.NewItem);
+        cy.url().should('contain', links.NewItem);
         cy.get('#jenkins-name-icon').click()
         
         cy.get('a[href="/asynchPeople/"]').click();
-        cy.url().should('eq', links.People);
+        cy.url().should('contain', links.People);
         
         cy.get('a[href="/view/all/builds"').click()
-        cy.url().should('eq', links.BuildHistory);
+        cy.url().should('contain', links.BuildHistory);
         
         cy.get('a[href="/manage"').click()
-        cy.url().should('eq', links.ManageJenkins);
+        cy.url().should('contain', links.ManageJenkins);
         
         cy.get('a[href="/me/my-views"]').click()
-        cy.url().should('eq', links.MyViews)
+        cy.url().should('contain', links.MyViews)
     })
 })
