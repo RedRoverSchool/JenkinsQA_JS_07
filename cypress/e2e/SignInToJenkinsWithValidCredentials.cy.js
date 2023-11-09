2a5daee297c29314a5888481fadfcfc4859babc7
Svetlana Koroleva <107718559+SvetlanaKoroleva@users.noreply.github.com>
2023-11-08 19:13:15 -0600

    modified 'Sign in to Jenkins' test

--- cypress/e2e/SignInToJenkinsWithValidCredentials.cy.js
+++ cypress/e2e/SignInToJenkinsWithValidCredentials.cy.js
@@ -2,6 +2,8 @@
 
 const USERNAME = Cypress.env('local.admin.username');
 const PASSWORD = Cypress.env('local.admin.password');
+const LOCAL_PORT = Cypress.env('local.port');
+const LOCAL_HOST = Cypress.env('local.host');
 
 describe('Sign in to Jenkins', () => {
 
@@ -10,7 +12,7 @@ describe('Sign in to Jenkins', () => {
        cy.get('#j_username').type(USERNAME)
        cy.get('#j_password').type(PASSWORD)
        cy.get('button[type = "submit"]').click()
-       cy.url().should('eq', `http://${LOCAL_HOST}:${LOCAL_PORT}`)
+       cy.url().should('eq', `http://${LOCAL_HOST}:${LOCAL_PORT}/`)
        cy.get('h1').should('have.text', 'Welcome to Jenkins!')
     })
 })
--- cypress/e2e/VerifyLeftSidePanel.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types = "cypress"/> 
-
-describe ('Verify items onside panel', () => {
-    it ('Verify all items on left side panel', function() {
-       cy.get('a') 
-
-    })
-})
