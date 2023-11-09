7dd3401a61db813741f8eb7df64a1fc4531daa31
Svetlana Koroleva <107718559+SvetlanaKoroleva@users.noreply.github.com>
2023-11-08 19:34:26 -0600

    modified 'Verify Jenkins Logo' test

--- cypress/e2e/VerifyJenkinsLogoInWelcomePage.cy.js
+++ cypress/e2e/VerifyJenkinsLogoInWelcomePage.cy.js
@@ -7,9 +7,9 @@ describe ('Verify Jenkins Logo', () => {
 
     it ('Verify Jenkins Logo and Title on welcome page is visible and clickable', function() {
         cy.get('#jenkins-name-icon').should('be.visible')
-        cy.get('#jenkins-name-icon').should('have.attr', 'src', '/static/f4fd4ca7/images/title.svg')
+        cy.get('#jenkins-name-icon').should('have.attr', 'src')
         cy.get('#jenkins-name-icon').click
-        cy.url().should('eq', 'http://localhost:8080/')
+        cy.url().should('eq', `http://${LOCAL_HOST}:${LOCAL_PORT}/`)
         cy.get('h1').should('have.text', 'Welcome to Jenkins!')
     })
 })
