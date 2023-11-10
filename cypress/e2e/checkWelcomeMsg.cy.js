07e7ba3c306069ceb6a6549799beb640870a8a83
Anna Sevalneva <annaanevik@gmail.com>
2023-11-10 12:02:24 +0100

    fix TC name, port issue, edit fixture usage

--- cypress/e2e/checkWelcomeMsg.cy.js
+++ cypress/e2e/checkWelcomeMsg.cy.js
@@ -1,11 +1,14 @@
 /// <reference types='cypress'/>
 
-import dashboardWelcomedMessage from '../fixtures/dashboardWelcomedMessage.json'
+const PORT = '8080'
 
 describe('checkWelcomeMsg', () => {
-  it('TC_02.03_018 | Dashboard > Welcome message', () => {
-    cy.url().should('be.equal', 'http://localhost:8080/')
-    cy.get('h1').should('be.visible').should('have.text', dashboardWelcomedMessage.pageTitle)
-    cy.get('h1').next('p').should('be.visible').should('have.text', dashboardWelcomedMessage.belowTitle)
-    });
+  it('TC_02.03.018 | Dashboard > Welcome message', () => {
+    const expectedUrl = `http://localhost:${PORT}/`;
+    cy.url().should('be.equal', expectedUrl)
+    cy.fixture('../fixtures/dashboardWelcomedMessage.json').then((dashboardWelcomedMessage) => {
+      cy.get('h1').should('be.visible').should('have.text', dashboardWelcomedMessage.pageTitle)
+      cy.get('h1').next('p').should('be.visible').should('have.text', dashboardWelcomedMessage.belowTitle)
+    })
+  });
 });
