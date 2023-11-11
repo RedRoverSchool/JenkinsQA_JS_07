57e64eddfc51a15c1d5441b6e5d85274dd6f324a
Ilia Khudiakov <ilia.khudiakov97@gmail.com>
2023-11-11 17:39:58 -0600

    modify 'Verify Jenkins Logo' test

--- cypress/e2e/VerifyJenkinsLogoInWelcomePage.cy.js
+++ cypress/e2e/VerifyJenkinsLogoInWelcomePage.cy.js
@@ -1,5 +1,6 @@
 /// <reference types = "cypress"/>
 
+import welcomeMessage from "../fixtures/homePageWelcomeMessage.json"
 const LOCAL_PORT = Cypress.env('local.port');
 const LOCAL_HOST = Cypress.env('local.host');
 
@@ -8,8 +9,8 @@ describe ('Verify Jenkins Logo', () => {
     it ('TC_02.02.008 | Verify Jenkins Logo and Title on welcome page is visible and clickable', function() {
         cy.get('#jenkins-name-icon').should('be.visible')
         cy.get('#jenkins-name-icon').should('have.attr', 'src')
-        cy.get('#jenkins-name-icon').click
+        cy.get('#jenkins-name-icon').click()
         cy.url().should('eq', `http://${LOCAL_HOST}:${LOCAL_PORT}/`)
-        cy.get('h1').should('have.text', 'Welcome to Jenkins!')
+        cy.get('h1').should('have.text', welcomeMessage.greeting)
     })
 })
