54ef3e5299aa0d292b7076b86b665ba8ebf6c081
OlgaKorolenko <o.e.korolenko@gmail.com>
2023-11-11 18:42:41 +0300

    update test

--- cypress/e2e/configureNewPipelineProject.cy.js
+++ cypress/e2e/configureNewPipelineProject.cy.js
@@ -1,20 +1,22 @@
 /// <reference types="cypress"/>
 
-describe('configureNewPipelineProject', () => {
+import createPipelineProject4 from"../fixtures/createPipelineProject4.json";
+import newJobPage from"../fixtures/newJobPage.json";
 
+describe('configureNewPipelineProject', () => {
+    
     beforeEach(() => {
-        cy.get('a[href="newJob"]').click()
-            cy.get('input#name').type('Pipeline Project1')
-            cy.get('li[tabindex="0"] span').contains('Pipeline').click()
-            cy.get('#ok-button').click()
+        cy.get('a[href="newJob"]').click();
+        cy.get('input#name').type(createPipelineProject4.newProjectName);
+        cy.get('li[tabindex="0"] span').contains(newJobPage.expectedProjectLabels[1]).click();
+        cy.get('#ok-button').click();
     });
     
-    it('TC_03.05.002 | New Item > Create Pipeline Project > Configure new Pipeline project>Verify Enable/Disable the current project', () => {
-        
+    it('TC_03.05.002 | New Item > Create Pipeline Project > Configure new Pipeline project>Verify Enable/Disable the current project', () => {        
         cy.get('#toggle-switch-enable-disable-project').click();       
         cy.wait(1000);
         cy.get('button[formnovalidate = "formNoValidate"]').click();
 
-        cy.get('#enable-project').should('have.text','\n          This project is currently disabled\n          \n    Enable\n  '); 
+        cy.get('#enable-project').should('contain','This project is currently disabled'); 
     });      
 });
