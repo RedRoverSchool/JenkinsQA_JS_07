c42330a336f263a80b76aa33c958e9703eeb71a7
JackBlaaack <eugene.lapytko@gmail.com>
2023-11-11 12:07:38 +0300

    added new it

--- cypress/e2e/startBuildingYourSoftwareProjectTest.cy.js
+++ cypress/e2e/startBuildingYourSoftwareProjectTest.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 
 import { expectedProjectLabels } from "../fixtures/newJobPage.json";
-
+import name from '../fixtures/newJobPage.json'
 describe("startBuildingYourSoftwareProjectTest", () => {
   it('TC_02.06.001 |Block “Start building your software project” > Verify "Start building your software project” is displayed', () => {
     cy.get('div.empty-state-block > :nth-child(3) h2').should("be.visible")
@@ -13,6 +13,12 @@ describe("startBuildingYourSoftwareProjectTest", () => {
     cy.url().should("include", "/newJob");
     cy.get(".h3").should("be.visible").and("have.text", "Enter an item name");
   });
+  it('TC_02.06.004 |Block “Start building your software project” >Check redirect to the page "New Job"', ()=>{
+
+    cy.get('a[href="newJob"]').click();
+    cy.url().should('be.eql',`http://localhost:${PORT}/newJob`);
+    cy.get('.h3').should('be.visible').and('have.text',name.fieldName) 
+    });
 
   it('TC_02.06.006 | Block "Start building your software project" > "Arrow" icon is visible and clickable', () => {
     cy.get('a[href="newJob"] svg').should("be.visible");
