ae675f4d1e289fc49a6067bd860a38c5c7578c3b
Nataly-Mayatskaya <130170775+Nataly-Mayatskaya@users.noreply.github.com>
2023-11-11 17:19:06 +0200

    nm/sign_in_with_invalid_userName (#213)

--- cypress/e2e/loginToJenkins.cy.js
+++ cypress/e2e/loginToJenkins.cy.js
@@ -1,10 +1,13 @@
 /// <reference types="cypress"/>
 
+import { loginErrorMessage } from "../fixtures/loginToJenkins.json"
+
 const port = Cypress.env('local.port');
 const host = Cypress.env('local.host');
 const validUserName = Cypress.env('local.admin.username');
 const validUserPass = Cypress.env('local.admin.password');
 const baseUrl = `http://${host}:${port}`
+let wrongUserName = Math.random().toString(36).substring(7)
 
 describe('Log in to Jenkins',() => {
     beforeEach(() => {
@@ -17,5 +20,14 @@ describe('Log in to Jenkins',() => {
         cy.get('.jenkins-button').click()
         cy.get('#jenkins-home-link').should('be.visible')
         cy.url().should('be.equal',`${baseUrl}/`)
+    });
+
+    it('TC_01.01.005 | Sign in to Jerkins > Sign in with invalid userName',() => {
+        cy.get('#j_username').type(wrongUserName)
+        cy.get('#j_password').type(validUserPass)
+        cy.get('#remember_me').check({ force: true })
+        cy.get('.jenkins-button').click()
+        cy.get('.app-sign-in-register__error').should('be.visible').and('have.text',loginErrorMessage)
+        cy.url().should('be.equal',`${baseUrl}/loginError`)
     });
 });
