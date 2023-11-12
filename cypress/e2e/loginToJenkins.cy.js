8a2f6ce46ee6d021ffab6393a17fc66fed5f5ee4
KateKazakovaJS <ackerman.levi1029384756@gmail.com>
2023-11-12 19:24:05 +0700

    Sign in with invalid password

--- cypress/e2e/loginToJenkins.cy.js
+++ cypress/e2e/loginToJenkins.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 
+import { it } from "mocha";
 import { loginErrorMessage } from "../fixtures/loginToJenkins.json"
 
 const port = Cypress.env('local.port');
@@ -8,6 +9,7 @@ const validUserName = Cypress.env('local.admin.username');
 const validUserPass = Cypress.env('local.admin.password');
 const baseUrl = `http://${host}:${port}`
 let wrongUserName = Math.random().toString(36).substring(7)
+let wrongPassword = Math.random().toString(36).substring(7)
 
 describe('Log in to Jenkins',() => {
     beforeEach(() => {
@@ -29,5 +31,12 @@ describe('Log in to Jenkins',() => {
         cy.get('.jenkins-button').click()
         cy.get('.app-sign-in-register__error').should('be.visible').and('have.text',loginErrorMessage)
         cy.url().should('be.equal',`${baseUrl}/loginError`)
+    });
+
+    it('TC_01.01.006 | Sign in to Jerkins > Sign in with invalid password', () => {
+        cy.get('#j_username').type(validUserName)
+        cy.get('#j_password').type(wrongPassword)
+        cy.get('button.jenkins-button').click()
+        cy.get('.app-sign-in-register__error').should('be.visible').and('have.text', loginErrorMessage)
     });
 });
