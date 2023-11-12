dd243308bf597646a8d4c06c4131aa605d1333bb
Ruslan Gudenko <47644886+RuslanG13@users.noreply.github.com>
2023-11-12 17:09:24 +0200

    RF | manageJenkinsTitle | TC_09.01.001 | Manage Jenkins > Title > Main header displays verification (#221)

--- cypress/e2e/manageJenkinsTitle.cy.js
+++ cypress/e2e/manageJenkinsTitle.cy.js
@@ -1,25 +1,29 @@
 /// <reference types="cypress"/>
 
 import manageJenkinsTitleData from "../fixtures/manageJenkinsTitle.json";
+import cypressEnvData from "../../cypress.env.json";
 
 describe("manageJenkinsTitle", () => {
     beforeEach(() => {
-        cy.get(".task-link-wrapper > a[href='/manage']").click();
+        cy.get("a[href='/manage']").click();
     })
 
     it("TC_09.01.001 | Manage Jenkins > Title > Main header displays verification", () => {
-        cy.url().should("include", manageJenkinsTitleData.manageJenkinsPageEndpoint);
+        const baseUrl = `http://${cypressEnvData["local.host"]}:${cypressEnvData["local.port"]}` + "/";
+        const manageJenkinsPageUrl = `${baseUrl}${manageJenkinsTitleData.manageJenkinsPageEndpoint}`;
 
-        cy.get(".jenkins-app-bar__content > h1").should("be.visible")
+        cy.url().should("be.eql", manageJenkinsPageUrl);
+
+        cy.get(".jenkins-app-bar__content h1").should("be.visible")
             .and("have.text", manageJenkinsTitleData.mainHeaderPage);
     });
 
     it("TC_09.01.002 | Manage Jenkins > Title > 5 section-headers displays verification", () => {
-        cy.get(".jenkins-section > h2").should("be.visible")
+        cy.get(".jenkins-section h2").should("be.visible")
             .then(($els) => {
                 return Cypress.$.makeArray($els).map($el => $el.innerText);
             })
             .and("have.length", 5)
-            .and("deep.equal", manageJenkinsTitleData.subHeadersMainJenkinsHeaders);
+            .and("deep.equal", manageJenkinsTitleData.subHeadersMainJenkinsPage);
     });
 }); 
