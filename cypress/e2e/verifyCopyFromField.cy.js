8cf0f5a7faed5f2963fb7f0be8ec56c0d23b6a27
Oxana Reshes <oxanareshes@gmail.com>
2023-11-13 09:02:22 -0800

    Verify 'Copy from' field

--- cypress/e2e/verifyCopyFromField.cy.js
+++ cypress/e2e/verifyCopyFromField.cy.js
@@ -1,25 +1,25 @@
 ///<reference types ="cypress"/>
-import { jobName, rendomJob } from "../fixtures/createNewJob.json";
+import createNewJob from "../fixtures/createNewJob.json";
 
 describe('Verify "Copy from" field', () => {
     beforeEach('Create new job', () => {
         cy.get("a[href='/view/all/newJob']").click()
         cy.get('label[for=name]').should('have.text', 'Enter an item name')
-        cy.get('input#name.jenkins-input').type(jobName)
-        cy.get('li[tabindex="0"] span').should('contain', 'Freestyle project').click({ multiple: true })
+        cy.get('input#name.jenkins-input').type(createNewJob.jobName)
+        cy.get('li[tabindex="0"] span').should('contain', createNewJob.projectName).click({ multiple: true })
         cy.get('#ok-button').click()
-        cy.get('.jenkins-app-bar__content > h1').should('have.text', 'Configuration')
+        cy.get('.jenkins-app-bar__content > h1').should('have.text', createNewJob.text)
         cy.get('.jenkins-button--primary ').click()
     })
     it("TC__03.08.09 | New Item > Verify 'Copy from' field >can't create a new item from none existing item", () => {
         cy.get("#jenkins-name-icon").click()
         cy.get("a[href='/view/all/newJob']").click()
-        cy.get('#name.jenkins-input').type('j')
+        cy.get('#name.jenkins-input').type(createNewJob.type)
         cy.get('div.item-copy').click()
-        cy.get('#from.jenkins-input.auto-complete').type(rendomJob)
+        cy.get('#from.jenkins-input.auto-complete').type(createNewJob.rendomJob)
         cy.get('#ok-button').click()
 
-        cy.get('h1').should('contain', 'Error')
+        cy.get('h1').should('have.text', createNewJob.message)
 
     })
 })
