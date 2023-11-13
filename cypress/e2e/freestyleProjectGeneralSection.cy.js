830fb48e65ab0b9425de3712539947c98633a993
Olga Safonova <vud.olya@gmail.com>
2023-11-13 12:41:10 -0500

    Added verification General section is displayed

--- cypress/e2e/freestyleProjectGeneralSection.cy.js
+++ cypress/e2e/freestyleProjectGeneralSection.cy.js
@@ -4,14 +4,27 @@ import freestyleProject from "../fixtures/freestyleProject.json"
 
 describe ('Freestyle Project', () => {
 
-    it('TC_04.01.011 | Freestyle project > Verify that General section is displayed on Configuration page', function(){
+    beforeEach('Create freestyle project', () => {
 
         cy.get('a[href= "/view/all/newJob"]').click();
         cy.get('input#name').type(freestyleProject.freestyleProjectName);
         cy.get('.hudson_model_FreeStyleProject').click();
         cy.get('#ok-button').click()
-     
+
+    })
+
+    it('TC_04.01.011 | Freestyle project > Verify that General section is displayed on Configuration page', function(){
+
         cy.get('[data-section-id="general"]').should('have.class', 'task-link--active')
         cy.get('#general').should('be.visible');
     })
+
+    it('TC_04.01.012 | Freestyle project > Verify that user is able to disable the project', function(){
+
+        cy.get('.jenkins-toggle-switch__label__checked-title').should('have.text', 'Enabled');
+        cy.get('#toggle-switch-enable-disable-project').click();
+
+        cy.get('.jenkins-toggle-switch__label__unchecked-title').should('have.text', 'Disabled');
+})
+
 })
