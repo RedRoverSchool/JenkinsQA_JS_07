77e80c00b2232083838224c0b0f0551f61f0bbdf
Nataly-Mayatskaya <mayatskaya.nataliya@gmail.com>
2023-11-13 11:11:59 +0200

    TC_07.03.002 | Display Name and Description fields

--- cypress/e2e/folderConfigure.cy.js
+++ cypress/e2e/folderConfigure.cy.js
@@ -6,8 +6,8 @@ const PORT = Cypress.env("local.port");
 import folderName from "../fixtures/createFolder.json";
 import sideMenu from "../fixtures/folderConfigure.json";
 
-describe("folderConfigure", () => {
-  beforeEach("createNewFolder", () => {
+describe("folderConfigure",() => {
+  beforeEach("createNewFolder",() => {
     cy.get("#tasks .task").contains(sideMenu.sidePanelHomePage).click();
     cy.get("#name").type(folderName.testfolderName);
     cy.get(".com_cloudbees_hudson_plugins_folder_Folder").click();
@@ -15,7 +15,7 @@ describe("folderConfigure", () => {
     cy.get('button[name="Submit"]').click();
   });
 
-  it("TC_07.03.001 | Folder > Configure > Verify link “Configure” on the folder page", () => {
+  it("TC_07.03.001 | Folder > Configure > Verify link “Configure” on the folder page",() => {
     cy.get("#tasks .task").contains(sideMenu.sidePanelFolderPage).click();
     cy.url().should(
       "equal",
@@ -24,6 +24,17 @@ describe("folderConfigure", () => {
     cy.get(".jenkins-breadcrumbs__list-item")
       .contains(sideMenu.breadcrumbsConfigurationPage)
       .should("be.visible")
-      .and("have.text", sideMenu.breadcrumbsConfigurationPage);
+      .and("have.text",sideMenu.breadcrumbsConfigurationPage);
+  });
+
+  it("TC_07.03.002 | Folder > Configure > The 'Display Name' and 'Description' fields in the General section are text input fields ",() => {
+    cy.get('#tasks .task').contains(sideMenu.sidePanelFolderPage).click();
+    cy.get('input.validated').type(folderName.displayName)
+    cy.get('textarea.jenkins-input').type(folderName.displayDescription)
+    cy.get('button[name="Submit"]').click()
+
+    cy.url().should('eql',`http://${HOST}:${PORT}/job/${folderName.testfolderName}/`)
+    cy.get('h1').should('be.visible').and('contain',folderName.displayName)
+    cy.get('#view-message').should('be.visible').and('have.text',folderName.displayDescription)
   });
 });
