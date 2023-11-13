95f68cdee9788ab62306698b5a80b42cd7328f36
SvetlanaKoroleva <svetlanakoroleva312@gmail.com>
2023-11-13 14:45:16 -0600

    added fixtures, modified code

--- cypress/e2e/creatingPipeLineProject.cy.js
+++ cypress/e2e/creatingPipeLineProject.cy.js
@@ -1,15 +1,19 @@
 /// <reference types="cypress"/>
 
-describe('pipelineProject', () => {
+describe('createPipelineProject', () => {
+    beforeEach(function() {
+        cy.fixture('creatingPipelineProject').then((projectInfo) => {
+            this.projectInfo = projectInfo;
+        }) 
+    })
 
-    it('TC_03.04.012 | New Item > Create Pipeline project', () => {
+    it('TC_03.04.012 | New Item > Create Pipeline project', function() {
         cy.get('a[href = "/view/all/newJob"]').click()
-        cy.get('input#name').type('firstProject')
+        cy.get('input#name').type(this.projectInfo.projectName)
         cy.get('li[tabindex="0"]').contains('Pipeline').click()
         cy.get('#ok-button').click()
         cy.get('a[href="/"]').contains('Dashboard').click()
-        cy.get('a[href="job/firstProject/"]').contains('firstProject').should('be.visible')
-        cy.get('a[href="job/firstProject/"]').contains('firstProject').should('have.text', 'firstProject')
+        cy.get('.jenkins-table__link').should('be.visible').and('have.text', this.projectInfo.projectName)
 
     })
 })
