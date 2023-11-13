7a1e8cdfe2f09d9a01c0f0a8c74f34d5eeaa20fa
Stepwow <stepanovcode@gmail.com>
2023-11-13 14:34:00 -0800

    vs/welcome_text

--- cypress/e2e/welcomeText.cy.js
+++ cypress/e2e/welcomeText.cy.js
@@ -1,11 +1,9 @@
 ///<reference types ="cypress"/>
+import { welcomeMessage } from '../fixtures/welcomePageVerification.json';
 
 describe('welcomeText', () => {
     
-    it('verify Welcome to Jenkins massage', function () {
-        cy.get('.empty-state-block h1').should('be.visible').and('have.text', 'Welcome to Jenkins!')
-    }) 
-    it('verify Explenation text', function () {
-        cy.get('.empty-state-block p').should('be.visible').and('have.text', 'This page is where your Jenkins jobs will be displayed. To get started, you can set up distributed builds or start building a software project.')
+    it('TC_02.03.019 Welcome to Jenkins massage verification', function () {
+        cy.get('.empty-state-block h1').should('be.visible').and('have.text', welcomeMessage);
     }) 
 })
