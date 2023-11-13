684e02cc4658e7d961055362f79f55ecb6544848
RuslanG13 <ruslan.gudenko13@ukr.net>
2023-11-13 05:35:26 +0200

    TC_03.07.007 | New Item > Create Multiconfiguration project > Configure new Multiconfiguration project > Verify that the left side 'Configure' menu with title and items is displayed

---
+++ cypress/e2e/configureNewMulticonfigurationProject.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+
+import confNewMultiConfProjData from "../fixtures/configureNewMulticonfigurationProject.json"
+import { configPageName } from "../fixtures/newItemFreestyleProject.json";
+import cypressEnvData from "../../cypress.env.json";
+
+describe("configureNewMulticonfigurationProject", () => {
+
+    const multiConfProjName = confNewMultiConfProjData.newMultiConfProjName + Date.now()
+    const baseUrl = `http://${cypressEnvData["local.host"]}:${cypressEnvData["local.port"]}`;
+    const confProjPageUrl = `${baseUrl}/${confNewMultiConfProjData.jobEndpoint}/${multiConfProjName}/${confNewMultiConfProjData.confProjPageEndpoint}`;
+
+    beforeEach(() => {
+        cy.get("a[href='/view/all/newJob']").click();
+        cy.get("input#name").type(multiConfProjName);
+        cy.get(".hudson_matrix_MatrixProject").click().should("have.attr", "aria-checked", "true");
+        cy.get("#ok-button").click();
+        cy.url().should("be.eql", confProjPageUrl);
+    });
+
+    it("TC_03.07.007 | Configure new Multiconfiguration project > Verify that the left side 'Configure' menu with items is displayed", () => {
+        cy.get(".jenkins-app-bar__content h1").should("be.visible").then($el => {
+            expect($el.text()).to.be.eql(configPageName);
+        });
+
+        cy.get("div#tasks").should("be.visible");
+    });
+});
