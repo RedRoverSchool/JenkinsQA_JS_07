413aded33de06cc39df05b169b0cb0283e77b827
Ksenyia Kravchuk <kkravchuk@uma.tech>
2023-11-12 19:58:32 -0500

    Move newItemPage key in newItemJson back

---
+++ cypress/e2e/freestyleProjectConfigure.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress"/>
+
+describe('Freestyle > Source Code Management section', () => {
+    beforeEach(() => {
+        
+    });
+
+    it('', () => {
+        
+    });
+});
