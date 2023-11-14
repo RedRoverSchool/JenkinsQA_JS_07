ef8a9166875c7b080afa5b8d5e82c33af2bb5854
Natalya Vasylyeva <natalya.vasylyeva.81@gmail.com>
2023-11-14 19:48:16 +0100

    TC_09.14.009 | Manage Jenkins > Security> Create User > Verify created User on the "Users [Jenkins]" page

--- cypress/e2e/manageJenkinsCreateUser.cy.js
+++ cypress/e2e/manageJenkinsCreateUser.cy.js
@@ -24,4 +24,22 @@ describe(" Manage Jenkins > Security> Create User", () => {
             );
         })
     });
+
+  it('TC_09.14.009 | Manage Jenkins > Security> Create User > Verify created User on the "Users [Jenkins]" page', () => {
+    cy.get("#username").clear().type(manageJenkinsCreateUserData.userCredentials.Username);
+    cy.get("input[name='password1']")
+      .clear()
+      .type(manageJenkinsCreateUserData.userCredentials.Password);
+    cy.get("input[name='password2']")
+      .clear()
+      .type(manageJenkinsCreateUserData.userCredentials.ConfirmPassword);
+    cy.get("input[name='fullname']").clear().type(manageJenkinsCreateUserData.userCredentials.FullName);
+    cy.get("input[name='email']").clear().type(manageJenkinsCreateUserData.userCredentials["E-mailAddress"]);
+    cy.get("button[name='Submit']").click();
+
+    cy.get("#people a")
+      .contains(manageJenkinsCreateUserData.userCredentials.Username)
+      .should("be.visible")
+      .and("have.text", manageJenkinsCreateUserData.userCredentials.Username);
+  });
 });
