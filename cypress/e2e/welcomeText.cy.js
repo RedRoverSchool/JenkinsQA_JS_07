c888cc0f7b303d54e5e5076ba3d6b99d713a1ec8
Stepwow <stepanovcode@gmail.com>
2023-11-14 07:03:24 -0800

    changed TC number on 02.03.021

--- cypress/e2e/welcomeText.cy.js
+++ cypress/e2e/welcomeText.cy.js
@@ -7,7 +7,7 @@ describe('welcomeText', () => {
     it('TC_02.03.019 Welcome to Jenkins massage verification', function () {
         cy.get('.empty-state-block h1').should('be.visible').and('have.text', welcomeMessage);
     }) 
-    it('TC_02.03.0201 Dashboard Explanation text message', function () {
+    it('TC_02.03.021 Dashboard Explanation text message', function () {
         cy.get('.empty-state-block p').should('be.visible').and('have.text', explanationText);
     }) 
 })
