03a184c6d569095e80ae33b3a332a6a5ca329b52
Ruslan Gudenko <47644886+RuslanG13@users.noreply.github.com>
2023-11-14 02:16:26 +0200

    TC_03.07.007 | Configure new Multiconfiguration project > Verify that the left side 'Configure' menu with title and items is displayed (#233)
    * TC_03.07.007 | New Item > Create Multiconfiguration project > Configure new Multiconfiguration project > Verify that the left side 'Configure' menu with title and items is displayed
    
    * TC_03.07.007 | Configure new Multiconfiguration project > Verify that the left side 'Configure' menu with title and items is displayed
    
    * changed fixture field in the fixture and test
    
    * small syntax fix
    
    * small syntax fix
    
    * delete cypress env import

---
+++ cypress/e2e/configureNewMulticonfigurationProject.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress"/>
+
+import confNewMultiConfProjData from "../fixtures/configureNewMulticonfigurationProject.json";
+import { configPageName } from "../fixtures/newItemFreestyleProject.json";
+
+describe("configureNewMulticonfigurationProject", () => {
+
+    const multiConfProjName = confNewMultiConfProjData.newMultiConfProjName + Date.now();
+    const baseUrl = `http://${Cypress.env("local.host")}:${Cypress.env("local.port")}`;
+    const confProjPageUrl = `${baseUrl}/${confNewMultiConfProjData.jobPath}/${multiConfProjName}/${confNewMultiConfProjData.confProjPageEndpoint}`;
+
+    beforeEach(() => {
+        cy.get("a[href='/view/all/newJob']").click();
+        cy.get("#name").type(multiConfProjName);
+        cy.get(".hudson_matrix_MatrixProject").click().should("have.attr", "aria-checked", "true");
+        cy.get("#ok-button").click();
+        cy.url().should("be.eql", confProjPageUrl);
+    });
+
+    it("TC_03.07.007 | Configure new Multiconfiguration project > Verify that the left side 'Configure' menu with items is displayed", () => {
+        cy.get(".jenkins-app-bar__content h1").should("be.visible").then($el => {
+            expect($el.text()).to.be.eql(configPageName);
+        });
+
+        cy.get("#tasks").should("be.visible");
+    });
+});
