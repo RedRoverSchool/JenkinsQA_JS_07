77804a6159beddff7a3cb0a42be4474e65b0d0da
JackBlaaack <eugene.lapytko@gmail.com>
2023-11-14 16:22:03 +0300

    transfered beforeeach inside describe

--- cypress/e2e/redirectToConfigurePage.cy.js
+++ cypress/e2e/redirectToConfigurePage.cy.js
@@ -6,16 +6,16 @@ import title from "../fixtures/folderConfigure.json"
 const PORT = Cypress.env("local.port");
 const HOST = Cypress.env('local.host');
 
-beforeEach('Create folder',() => {
-    cy.get('span').contains('Create a job').click();
-    cy.get('#name').type(folderName.folderName);
-    cy.get('li.com_cloudbees_hudson_plugins_folder_Folder').scrollIntoView();
-    cy.wait(2000);
-    cy.get('li.com_cloudbees_hudson_plugins_folder_Folder').click();
-    cy.get('#ok-button').click();
-    cy.get('a[href="/job/JenkinsFolder/"]').click();
-});
 describe('redirect to congigure page', () => {
+    beforeEach('Create folder',() => {
+        cy.get('span').contains('Create a job').click();
+        cy.get('#name').type(folderName.folderName);
+        cy.get('li.com_cloudbees_hudson_plugins_folder_Folder').scrollIntoView();
+        cy.wait(2000);
+        cy.get('li.com_cloudbees_hudson_plugins_folder_Folder').click();
+        cy.get('#ok-button').click();
+        cy.get('a[href="/job/JenkinsFolder/"]').click();
+    });
 it('test', () => {
    cy.get('span').contains('Configure').click();
    cy.url().should('equal', `http://${HOST}:${PORT}/job/JenkinsFolder/configure`)
