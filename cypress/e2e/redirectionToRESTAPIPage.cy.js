942521d1057199c0428400cd954684747190fd1d
gulkinna <gulkinna@gmail.com>
2023-11-14 17:15:01 -0500

    fixed

--- cypress/e2e/redirectionToRESTAPIPage.cy.js
+++ cypress/e2e/redirectionToRESTAPIPage.cy.js
@@ -1,10 +1,13 @@
 /// <reference types="cypress"/>
 
+import api from '../fixtures/redirectionToRESTAPI.json'
+
 describe('redirectionToRESTAPIPage', () => {
     it('TC_15.01.002 | Footer> Access to REST API > Verify redirection to REST API page', () => {
         cy.get('.rest-api').click()
 
-        cy.get('#page-body h1').should('be.visible').and('have.text', 'REST API')
-        cy.get('.jenkins-breadcrumbs__list-item a[href="/api/"]').should('be.visible').and('have.text', 'API')
+        cy.url().should('include', api.url)
+        cy.get('#page-body h1').should('be.visible').and('have.text', api.REST_API)
+        cy.get('.jenkins-breadcrumbs__list-item a[href="/api/"]').should('be.visible').and('have.text', api.API)
     });
 });
