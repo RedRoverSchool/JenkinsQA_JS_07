ce7f40c7dba84348ee2970ae6121719ba923faf5
Deneshik <adeneshik@mail.ru>
2023-11-14 15:07:15 +0300

    adedd changes TC_02.02.009

--- cypress/e2e/logoAndTitle33.cy.js
+++ cypress/e2e/logoAndTitle33.cy.js
@@ -1,14 +1,11 @@
 /// <reference types="cypress"/>
 
-import { JenkinsHome } from '../fixtures/logoAndTitle.json'
-
 describe('logoAndTitle33', () => {
     
     it('TC_02.02.009 | Header > Jenkins Logo and Title', () => {
-        cy.visit('http://localhost:8080/');
         cy.get('#jenkins-home-link').should('be.visible').click(); 
         cy.get('#jenkins-name-icon').should('have.attr', 'src');   
-        cy.get('h1').should('have.text', JenkinsHome.frontPage).and('be.visible');
+        cy.get('a[href="/view/all/newJob"]').click();
     });
 
 });
