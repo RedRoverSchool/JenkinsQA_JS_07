7fd0ee336a0d873e44922c56ce9cde53209a99a6
gulkinna <gulkinna@gmail.com>
2023-11-14 08:48:26 -0500

    test added

--- cypress/e2e/redirectionToRESTAPIPage.cy.js
+++ cypress/e2e/redirectionToRESTAPIPage.cy.js
@@ -3,5 +3,8 @@
 describe('redirectionToRESTAPIPage', () => {
     it('TC_15.01.002 | Footer> Access to REST API > Verify redirection to REST API page', () => {
         cy.get('.rest-api').click()
+
+        cy.get('#page-body h1').should('be.visible').and('have.text', 'REST API')
+        cy.get('.jenkins-breadcrumbs__list-item a[href="/api/"]').should('be.visible').and('have.text', 'API')
     });
 });
