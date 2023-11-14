49198e9da1e606782ea947ddcd95d8a12afec27d
Deneshik <adeneshik@mail.ru>
2023-11-14 17:53:42 +0300

    added changed TC_02.02.009

--- cypress/e2e/logoAndTitle33.cy.js
+++ cypress/e2e/logoAndTitle33.cy.js
@@ -3,9 +3,8 @@
 describe('logoAndTitle33', () => {
     
     it('TC_02.02.009 | Header > Jenkins Logo and Title', () => {
-        cy.get('#jenkins-home-link').should('be.visible').click(); 
+        cy.get('#jenkins-home-link').should('be.visible'); 
         cy.get('#jenkins-name-icon').should('have.attr', 'src');   
-        cy.get('a[href="/view/all/newJob"]').click();
     });
 
 });
