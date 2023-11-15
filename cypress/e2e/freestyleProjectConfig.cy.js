e522a581adaa91117330fe3495a489a3952c0a6c
KravchukKseniya <39743588+KravchukKseniya@users.noreply.github.com>
2023-11-15 12:23:37 -0800

    TC_04.02.002 | Freestyle > Source Code Management > Verify there are two radio buttons (#251)

--- cypress/e2e/freestyleProjectConfig.cy.js
+++ cypress/e2e/freestyleProjectConfig.cy.js
@@ -2,7 +2,7 @@
 
 import data from "../fixtures/freestyleProjectConfiguration.json"
 
-describe('TC_04.02.001 | Freestyle > Source Code Management section', () => {
+describe('US_04.02 | Freestyle > Source Code Management section', () => {
     beforeEach(() => {
         cy.get('a[href="/view/all/newJob"]').click();
         cy.get('input#name').type(data.projectName);
@@ -13,5 +13,13 @@ describe('TC_04.02.001 | Freestyle > Source Code Management section', () => {
 
     it('TC_04.02.001 | Freestyle > Source Code Management > Source Code Management section is displayed', () => {
         cy.get('#source-code-management').should('be.visible').and('contain', data.sectionName);
+    });
+
+    it('TC_04.02.002 | Freestyle > Source Code Management > Verify there are two radio buttons', () => {
+        cy.get('#source-code-management').parent().as('section')
+        cy.get('@section').find('[id^=radio-block-]').should('have.length', 2);
+        cy.get('@section').find('label[for^=radio-block-]').each(($el, index) => {
+            expect($el.text()).to.be.eql(data.radioButtonNames[index])
+        });
     });
 });
