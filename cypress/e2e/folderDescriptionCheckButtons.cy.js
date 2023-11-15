0e5816d71f0a0e7413e061ca5e4513993d5e3426
Olga Safonova <vud.olya@gmail.com>
2023-11-15 16:41:20 -0500

    Added new TC >Folder>Description

---
+++ cypress/e2e/folderDescriptionCheckButtons.cy.js
@@ -0,0 +1,24 @@
+
+import folderCreation from "../fixtures/folderCreation.json";
+
+describe('Folder Description', () => {
+
+    beforeEach('Create new folder', () => {
+        cy.get('a[href= "/view/all/newJob"]').click();
+        cy.get('input#name').type(folderCreation.folderName);
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click();
+        cy.get('#ok-button').click();
+        cy.visit(`http://localhost:8080/job/${folderCreation.folderName}/`)
+
+
+    })
+
+    it('TC_07.02.003 | Verify that an empty input for text field appears when hit on “Add description” button', () => {
+
+        cy.url().should('be.equal', `http://localhost:8080/job/${folderCreation.folderName}/`)
+        cy.get('#description-link').click();
+
+        cy.get('.jenkins-input').should('exist')
+    })
+
+})
