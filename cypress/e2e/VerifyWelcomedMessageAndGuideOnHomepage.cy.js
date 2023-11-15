e1270bd0438b494ae16499ad3236c5db2f5e2aa4
Ilia Khudiakov <ilia.khudiakov97@gmail.com>
2023-11-15 17:07:34 -0600

    corrected 'Verify welcomed message' test

--- cypress/e2e/VerifyWelcomedMessageAndGuideOnHomepage.cy.js
+++ cypress/e2e/VerifyWelcomedMessageAndGuideOnHomepage.cy.js
@@ -3,8 +3,8 @@
 import welcomeMessage from "../fixtures/welcomeAndGuideMessage.json"
 import quideMessage from "../fixtures/welcomeAndGuideMessage.json"
 
-describe ('TC_02.03.015 | Verify welcomed message', () => {
-    it('Verify welcomed message and quick guide on homepage', function() {
+describe ( 'Verify welcomed message', () => {
+    it('TC_02.03.015 |Verify welcomed message and quick guide on homepage', function() {
         cy.get('h1').should('be.visible')
         cy.get('h1').should('have.text', welcomeMessage.greeting)
         cy.get('p').should('be.visible')
