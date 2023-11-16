09c9aedbd7f52299eb5f9349a13432e510625f99
Stasy-n <dacenko.anast@gmail.com>
2023-11-16 20:49:38 +0100

    Formating files

--- cypress/e2e/verifyLabelsAtNewJobPage.cy.js
+++ cypress/e2e/verifyLabelsAtNewJobPage.cy.js
@@ -1,19 +1,16 @@
 /// <reference types="cypress"/>
-import labels from "../fixtures/labelsAtnewJobPage.json"
+import labels from "../fixtures/labelsAtnewJobPage.json";
 
-describe('verifyLabelsAtNewJobPage', () =>{
-     beforeEach(function(){
-        cy.get('a[href="/view/all/newJob"]').click();
-       cy.get(".label").as("labelName");
+describe("verifyLabelsAtNewJobPage", () => {
+  beforeEach(function () {
+    cy.get('a[href="/view/all/newJob"]').click();
+    cy.get(".label").as("labelName");
+  });
+  
+  labels.labelName.forEach((name, index) => {
+    it(`TC_03.01.11 | + New Item > Verify label name ${name} on /newJob page`, function () {
+      cy.url().should("contain", labels.url);
+      cy.wrap(this.labelName[index]).contains(name);
     });
-    labels.labelName.forEach((name,index) => {
-    it(`TC_03.01.11 | + New Item > Verify label name ${name} on /newJob page`, function (){
-
-    cy.url().should("contain", labels.url);
-    cy.wrap(this.labelName[index]).contains(name);
-    });
+  });
 });
-});
-
-
-    
