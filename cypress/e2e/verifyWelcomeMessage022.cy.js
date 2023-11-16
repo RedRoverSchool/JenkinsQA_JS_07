9300925b95389b7198ab7750eaa85e257505bd07
PMG2112 <pmgimperia@gmail.com>
2023-11-16 08:29:30 -0600

    added TC_02.03.022

--- cypress/e2e/verifyWelcomeMessage022.cy.js
+++ cypress/e2e/verifyWelcomeMessage022.cy.js
@@ -4,6 +4,7 @@ import WelcomeToJenkins from "../fixtures/WelcomeToJenkins.json"
 
 describe('WelcomeToJenkins', () => {
    it('TC_02.03.022 | Dashboard > Verify  Welcomed message on the main page', () =>{
-    cy.get('div[class="empty-state-block"]').should('have.text', WelcomeToJenkins.mainPageTitle);
+      cy.get('div[class="empty-state-block"]>h1').should('have.text', WelcomeToJenkins.mainPageTitle);
+      cy.get('div[class="empty-state-block"]').contains(WelcomeToJenkins.mainPageTitle).should('have.attr', h1);
    })
 });
