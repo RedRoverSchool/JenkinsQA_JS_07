03cd3af6bd4eacaa0e79010e724e62760a434f25
SvetlanaKazakova <mks.kazakova@mail.ru>
2023-11-16 12:22:03 +0300

    add

--- cypress/e2e/dashboardLinkSideMenu.cy.js
+++ cypress/e2e/dashboardLinkSideMenu.cy.js
@@ -5,7 +5,7 @@ describe('dashboardLinkSideMenu',()=>{
         it(`TC_02.04.25|Dashboard>Link Side menu| Verify side menu link ${dbLinkSideMenu.sidePanelName[ind]}`,()=>{
             cy.get('#side-panel #tasks span a').eq(ind).click()
             cy.url('contains',dbLinkSideMenu.sidePanelLink[ind])
-            cy.contains(dbLinkSideMenu.pageHeaderName[ind])
+            cy.contains(dbLinkSideMenu.pageHeaderName[ind]);
         })
     })
 })
