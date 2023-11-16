996f8d03941e1ef3451e4449abb708981134f8d2
AnastasiiaGri <anastasiiagrigorian@gmail.com>
2023-11-15 19:02:39 -0800

    add correct endpoint

--- cypress/e2e/createFreestyleProject3.cy.js
+++ cypress/e2e/createFreestyleProject3.cy.js
@@ -1,16 +1,16 @@
 /// <reference types="cypress"/>
-import freestyleProject from '../fixtures/freestyleProject.json'
+import createFreestyleProject2 from '../fixtures/createFreestyleProject2.json'
 
 describe ('Ceate Fressstyle Project', () => {
 
     it('TC_03.02.001 | New item > Create Freestyle Project > new Freestyle Project configuration page is displayed', function () {
 
         cy.get('a[href="/view/all/newJob"]').click()
-        cy.get('#name').type(freestyleProject.freestyleProjectName)
+        cy.get('#name').type(createFreestyleProject2.projectName)
         cy.get('.hudson_model_FreeStyleProject').click()
         cy.get('#ok-button').click()
 
-        cy.url().should('contain', 'configure')
+        cy.url().should('contain', `/job/${createFreestyleProject2.projectName}/configure`)
         cy.get('#general').should('have.text', "General")
 
     }); 
