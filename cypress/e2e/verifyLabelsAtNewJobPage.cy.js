a37053d358e9ea231772cca8f361a941d3ba3084
Stasy-n <dacenko.anast@gmail.com>
2023-11-16 16:01:27 +0100

    add test for verifying label names on /newJob page

---
+++ cypress/e2e/verifyLabelsAtNewJobPage.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+import labels from "../fixtures/labelsAtnewJobPage.json"
+
+describe('verifyLabelsAtNewJobPage', () =>{
+     beforeEach(function(){
+        cy.get('a[href="/view/all/newJob"]').click();
+       cy.get(".label").as("labelName");
+    });
+    labels.labelName.forEach((name,index) => {
+    it(`TC_03.01.11 | + New Item > Verify label name ${name} on /newJob page`, function (){
+
+    cy.url().should("contain", labels.url);
+    cy.wrap(this.labelName[index]).contains(name);
+    });
+});
+});
+
+
+    
