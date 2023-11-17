d13e571935497a0360ca3817232d935ec4749c30
gulkinna <gulkinna@gmail.com>
2023-11-17 14:46:58 -0500

    pom

---
+++ cypress/e2e/pom_e2e/redirectionToRestApiPage.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress"/>
+import HomePage from "../../pageObjects/HomePage";
+import RestApiPage from "../../pageObjects/RestApiPage";
+import api from "../../fixtures/pom_fixtures/restAPI.json"
+
+describe("redirectionToRESTAPIPage", () => {
+  const homePage = new HomePage();
+  const restApi = new RestApiPage();
+
+  it("Verify redirection to REST API page", () => {
+    homePage.clickgetRestApilink();
+
+    restApi
+        .getUrl()
+        .should("include", api.link);
+    restApi
+        .getApiHeaderText()
+        .should("be.visible")
+        .and("have.text", api.header);
+    restApi
+        .getBreadcrumbsApiLink()
+        .should("be.visible")
+        .and("have.text", api.API);
+  });
+});
