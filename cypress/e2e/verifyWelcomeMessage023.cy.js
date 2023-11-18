a182ab148d4043fce1d20f61e688d4124f17cd9e
PMG2112 <pmgimperia@gmail.com>
2023-11-17 22:28:06 -0600

    added verify the text size

--- cypress/e2e/verifyWelcomeMessage023.cy.js
+++ cypress/e2e/verifyWelcomeMessage023.cy.js
@@ -4,7 +4,8 @@ import WelcomeToJenkins from "..//fixtures/welcomeMessage023.json"
 
 describe('verifyWelcomeMessage023', () => {
    it('TC_02.03.022 | Dashboard > Verify  Welcomed message on the main page', () =>{
-      cy.get('div[class="empty-state-block"]').find('h1').should('have.text', WelcomeToJenkins.mainMessage);
-      cy.get('div.empty-state-block > p').should('be.visible').and('have.text', WelcomeToJenkins.helpMessage);
+      cy.get('div[class="empty-state-block"]>h1').should('have.text', WelcomeToJenkins.mainMessage);
+      cy.contains(WelcomeToJenkins.mainMessage).and("have.prop", "tagName", "H1");
+      cy.get('div.empty-state-block > p').should('have.text', WelcomeToJenkins.helpMessage);
    })
 });
