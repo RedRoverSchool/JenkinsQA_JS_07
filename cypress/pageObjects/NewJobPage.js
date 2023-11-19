530551b805519afa23e74e59f119a045d0475264
Ilia Khudiakov <ilia.khudiakov97@gmail.com>
2023-11-18 20:27:39 -0600

    modify pageObject file

--- cypress/pageObjects/NewJobPage.js
+++ cypress/pageObjects/NewJobPage.js
@@ -5,6 +5,7 @@ class NewJobPage {
     getFreestyleProjectBtn = () => cy.get('li.hudson_model_FreeStyleProject');
     getPipelineBtn = () => cy.get('li.org_jenkinsci_plugins_workflow_job_WorkflowJob');
     getMultiConfigTypeOfProjectBtn = () => cy.get('li.hudson_matrix_MatrixProject');
+    getOKButton = () => cy.get('#ok-button');
     getFolderBtn =() => cy.get('li.com_cloudbees_hudson_plugins_folder_Folder')
     getMultibranchPipelineBtn = () => cy.get('li.org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject')
     getOrganizationFolder = () => cy.get('li.jenkins_branch_OrganizationFolder');
