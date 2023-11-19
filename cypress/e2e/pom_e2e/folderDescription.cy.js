5fd09d862cb76baabbc5ffb489ec60a439140c2a
ElenaDetusheva <148974974+ElenaDetusheva@users.noreply.github.com>
2023-11-19 21:18:27 +0300

    pom_TC_07.02.006 (#299)

---
+++ cypress/e2e/pom_e2e/folderDescription.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+
+import HomePage from "../../pageObjects/HomePage";
+import folderConfigureData from "../../fixtures/pom_fixtures/folderConfigureData.json";
+import FolderPage from "../../pageObjects/FolderPage";
+import folderPageData from "../../fixtures/pom_fixtures/folderPageData.json";
+
+describe("folderDescription", () => {
+  const homePage = new HomePage();
+  const folderPage = new FolderPage();
+
+  beforeEach("createNewFolder", () => {
+    homePage
+      .clickNewItemLink()
+      .fillInputNameField(folderConfigureData.folderName)
+      .clickFolderBtn()
+      .clickOKButtonFolder()
+      .clickSaveBtn();
+  });
+
+  it("TC_07.02.006 | Verify that an empty input for text field appears when hit on “Add description” button", function () {
+    folderPage
+      .clickAddDescriptionLink()
+      .getPreviewLink()
+      .should("be.visible")
+      .and("have.text", folderPageData.previewLinkName);
+  });
+});
--- cypress/pageObjects/FolderPage.js
+++ cypress/pageObjects/FolderPage.js
@@ -2,11 +2,18 @@ import FolderConfigurePage from "./FolderConfigurePage";
 
 class FolderPage {
   getConfigureLink = () => cy.get("a[href*='configure']");
-
+  getAddDescriptionLink = () => cy.get("#description-link");
+  getPreviewLink = () => cy.get('a.textarea-show-preview');
   clickConfigureLink() {
     this.getConfigureLink().click();
 
     return new FolderConfigurePage();
+  }
+
+  clickAddDescriptionLink() {
+    this.getAddDescriptionLink().click();
+
+    return this;
   }
 }
 
