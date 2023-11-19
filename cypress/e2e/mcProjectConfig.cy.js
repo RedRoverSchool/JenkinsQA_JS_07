c99eebf99afeb9468ee77083437493cd87c942b8
Vladimir Vakulenko <vakulenko.volodia@yandex.ru>
2023-11-19 18:56:21 +0200

    Added test  MC project configuration 19.11.23

--- cypress/e2e/mcProjectConfig.cy.js
+++ cypress/e2e/mcProjectConfig.cy.js
@@ -11,14 +11,14 @@ describe('mcProjectConfig', () => {
         cy.get('#ok-button').click()
     })
 
-    it('TC_03.07.011 | Configure new multiconfiguration project > Add description', function() {
+    it('TC_03.07.011.1 | Configure new multiconfiguration project > Add description', function() {
         cy.get('div.jenkins-section textarea.jenkins-input').type(projectDescription.currentDescription)
         cy.get('button[name="Submit"]').click()
 
         cy.get('#description > div:nth-child(1)').should('have.text', projectDescription.currentDescription)
     });
 
-    it('TC_03.07.011 | Configure new multiconfiguration project > Check box test ', function() {
+    it('TC_03.07.011.2 | Configure new multiconfiguration project > Check box test ', function() {
         cy.get('#cb8').check({force: true}).should('be.checked')
         
         cy.get('#cb4').should('not.be.checked')
