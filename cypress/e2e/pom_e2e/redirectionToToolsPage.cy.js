26643f560e3dd0460e31ded2adb24107ef3c2284
Oxana Reshes <oxanareshes@gmail.com>
2023-11-20 19:14:45 -0800

    redirection to Tools page

--- cypress/e2e/pom_e2e/redirectionToToolsPage.cy.js
+++ cypress/e2e/pom_e2e/redirectionToToolsPage.cy.js
@@ -1,18 +1,14 @@
 /// <reference types="cypress"/>
-import ToolsPage from "../../pageObjects/ToolsPage";
 import HomePage from "../../pageObjects/HomePage";
-import ManageJenkinsPage from "../../pageObjects/ManageJenkinsPage"
 import configureToolsTitle from "../../fixtures/pom_fixtures/configureToolsTitle.json"
 
 describe('System Configuration', () => {
-    const homePage = new HomePage();
-    const manageJenkins = new ManageJenkinsPage();
-    const toolsPage = new ToolsPage
+  const homePage = new HomePage();
 
-    it('TC__09.06.02 | Verify redirection to Tools Page', ()=> {
-    homePage.clickManageJenkinsLink();
-    manageJenkins.clickToolsLink();
-    toolsPage.getToolsTitle()
-             .should('have.text',configureToolsTitle.Title)
-  })  
+  it('TC__09.06.02 | Verify redirection to Tools Page', () => {
+    homePage.clickManageJenkinsLink()
+      .clickToolsLink()
+      .getToolsTitle()
+      .should('have.text', configureToolsTitle.Title)
+  })
 })
