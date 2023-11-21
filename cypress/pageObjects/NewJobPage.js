7677dfc46e19551b245d4ea57bc7e02b5ee75e78
JackBlaaack <eugene.lapytko@gmail.com>
2023-11-21 12:46:43 +0300

    changed files

--- cypress/pageObjects/NewJobPage.js
+++ cypress/pageObjects/NewJobPage.js
@@ -1,6 +1,8 @@
 import MultiConfigProjectConfigurePage from "../pageObjects/MultiConfigProjectConfigurePage";
 import FreestyleProjectConfigurePage from "../pageObjects/FreestyleProjectConfigurePage";
 import FolderConfigurePage from "../pageObjects/FolderConfigurePage"
+const HOST = Cypress.env('local.host');
+const PORT = Cypress.env('local.port');
 class NewJobPage {
     getInputNameField = () => cy.get('input#name');
     getMultiConfigTypeOfProjectBtn = () => cy.get('li.hudson_matrix_MatrixProject');
@@ -9,6 +11,8 @@ class NewJobPage {
     getTypeOfProjectLabels = () => cy.get(".label");
     getFolderBtn = () => cy.get(".com_cloudbees_hudson_plugins_folder_Folder");
     getPipelineProjectNameLink = () => cy.get('.label');
+    getNewJobPageUrl = () => cy.url();
+    getNewJobPageTitle = () => cy.get('label[for="name"]');
     
     fillInputNameField(nameProject) {
         this.getInputNameField().should('be.visible').type(nameProject);
@@ -62,6 +66,9 @@ class NewJobPage {
         this.getPipelineProjectNameLink().click
         
         return this
+      }
+      checkNewJobPageUrl() {
+        this.getNewJobPageUrl().should('be.eql',`http://${HOST}:${PORT}/view/all/newJob`)
       }
  
 }
