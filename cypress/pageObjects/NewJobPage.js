c383934daa7d58ac3089ecff395bf658f7e7926b
SvetlanaKoroleva <svetlanakoroleva312@gmail.com>
2023-11-20 19:46:13 -0600

    added pom pipeline project

--- cypress/pageObjects/NewJobPage.js
+++ cypress/pageObjects/NewJobPage.js
@@ -1,10 +1,12 @@
 import MultiConfigProjectConfigurePage from "../pageObjects/MultiConfigProjectConfigurePage";
 import FreestyleProjectConfigurePage from "../pageObjects/FreestyleProjectConfigurePage";
+import PipelineProjectConfigurePage from "../pageObjects/PipelineProjectConfigurePage";
 import FolderConfigurePage from "../pageObjects/FolderConfigurePage"
 class NewJobPage {
     getInputNameField = () => cy.get('input#name');
     getMultiConfigTypeOfProjectBtn = () => cy.get('li.hudson_matrix_MatrixProject');
     getOKButton = () => cy.get('#ok-button');
+    getPipelineTypeofProjectBtn = () => cy.get('li[tabindex="0"]').contains('Pipeline').click();
     getFreestyleTypeOfProjectBtn = () => cy.get('.hudson_model_FreeStyleProject');
     getTypeOfProjectLabels = () => cy.get(".label");
     getFolderBtn = () => cy.get(".com_cloudbees_hudson_plugins_folder_Folder");
@@ -26,6 +28,20 @@ class NewJobPage {
 
         return new MultiConfigProjectConfigurePage();
     }
+
+    PipelineTypeofProjectBtn() {
+        this.getPipelineTypeofProjectBtn().click();
+
+        return this;
+    }
+
+    clickOKButtonPipeline() {
+        this.getOKButton().click();
+
+        return new PipelineProjectConfigurePage();
+    }
+
+
 
     clickFreestyleTypeOfProjectBtn() {
         this.getFreestyleTypeOfProjectBtn().click();
