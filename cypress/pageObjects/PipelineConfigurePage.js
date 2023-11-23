f2b4dcc657ddebcd235ac921e84a4264a49057c3
OlgaKorolenko <o.e.korolenko@gmail.com>
2023-11-23 21:00:53 +0300

    RF | POM >TC_03.05.002

--- cypress/pageObjects/PipelineConfigurePage.js
+++ cypress/pageObjects/PipelineConfigurePage.js
@@ -1,7 +1,8 @@
 class PipelineConfigurePage {
     getEnabDisabSwitchToggle = () => cy.get('#toggle-switch-enable-disable-project');
     getLabelDisabled = () => cy.get('.jenkins-toggle-switch__label__unchecked-title');
-    getLabelEnabled = () => cy.get('.jenkins-toggle-switch__label__checked-title')
+    getLabelEnabled = () => cy.get('.jenkins-toggle-switch__label__checked-title');
+    getSaveBtn = () => cy.get('.jenkins-button--primary')
 
     clickEnabDisabSwitchToggle(){
         this.getEnabDisabSwitchToggle().click();
@@ -9,5 +10,11 @@ class PipelineConfigurePage {
         return this;
     }
 
+    clickSaveBtn(){
+        this.getSaveBtn().click();
+
+        return this;
+    }
+    
 }
 export default  PipelineConfigurePage
