7c5f6e94bb4e444442850e71215193ac730fec76
ElenaDetusheva <e.det@list.ru>
2023-11-23 11:51:05 +0300

    add_tc_07_02_10

--- cypress/pageObjects/FolderPage.js
+++ cypress/pageObjects/FolderPage.js
@@ -4,6 +4,8 @@ class FolderPage {
   getConfigureLink = () => cy.get("a[href*='configure']");
   getAddDescriptionLink = () => cy.get("#description-link");
   getPreviewLink = () => cy.get('a.textarea-show-preview');
+  getTextAreaPrewew = () => cy.get('div.textarea-preview');
+  getHidePreviewLink = () => cy.get('a.textarea-hide-preview');
   getDisplayFolderName = () => cy.get('h1');
   getDiscriptionFolderMessage = () => cy.get('#view-message');
   getInputField  =() => cy.get('.jenkins-input');
@@ -13,7 +15,7 @@ class FolderPage {
   getPropertiesAddBtn = () => cy.get('button[class="jenkins-button repeatable-add"]');
   getSidePanelLinks = () => cy.get('div.task>.task-link-wrapper>.task-link');
   
-  
+ 
   clickConfigureLink() {
     this.getConfigureLink().click();
 
@@ -37,6 +39,11 @@ clickPropertiesAddBtn() {
 
   typeInputField(text){
     this.getInputField().type(text);
+    return this;
+  }
+
+  clickPreviewLink() {
+    this.getPreviewLink().click();
     return this;
   }
 
