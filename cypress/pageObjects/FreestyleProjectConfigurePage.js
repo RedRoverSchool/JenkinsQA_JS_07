d5d907a7524a98e53267248147dca251c3ba367e
KravchukKseniya <39743588+KravchukKseniya@users.noreply.github.com>
2023-11-24 13:01:47 -0800

    RF | POM > TC_04.02.004 | Freestyle > Source Code Management > Git option has a tooltip (#330)

--- cypress/pageObjects/FreestyleProjectConfigurePage.js
+++ cypress/pageObjects/FreestyleProjectConfigurePage.js
@@ -3,10 +3,12 @@ class FreestyleProjectConfigurePage {
     getSrcCodeMngmntNoneOption = () => cy.get('#radio-block-0');
     getSourceCodeManagementSectionBtns = () => cy.get('#source-code-management').parent().find('[id^=radio-block-]');
     getSourceCodeManagementSectionLbls = () => cy.get('#source-code-management').parent().find('label[for^=radio-block-]');
+    getGitOptionTooltip = () => cy.get('a[tooltip="Help for feature: Git"]');
+    getGitOptionTooltipContent = () => cy.get('div.tippy-box');
 
     clickSourceCodeManagementMenuItem() {
         this.getSourceCodeManagementSection().click();
         return this;
-    }
+    };
 }
 export default FreestyleProjectConfigurePage;
