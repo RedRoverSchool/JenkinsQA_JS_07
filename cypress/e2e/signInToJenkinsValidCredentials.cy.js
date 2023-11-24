a4bdfbcc341192e58a5cc0ca58ccd50a2d9b00e4
IuliiaGera <sckrafftykt@gmail.com>
2023-11-23 18:37:30 -0600

    1

---
+++ cypress/e2e/signInToJenkinsValidCredentials.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('signInToJenkinsValidCredentials', () => {
+    
+    it('Sign In with valid credentials', () => {
+        
+        cy.visit('http://localhost:8080/login ')
+        cy.get('#j_username').type('admin')
+        cy.get('#j_password').type('e7ff746b4cc745aba0d8d96f617e0517')
+        cy.get('.jenkins-button').click()
+        
+        cy.get('h1').should('have.text', 'Welcome to Jenkins!')
+
+    });
+});
