663e0dc335504b79ecb2521176c56991b8153812
Olga Safonova <vud.olya@gmail.com>
2023-11-24 16:32:04 -0500

    create new tc FreestyleGeneralSection

--- cypress/pageObjects/FreestyleProjectConfigurePage.js
+++ cypress/pageObjects/FreestyleProjectConfigurePage.js
@@ -1,9 +1,18 @@
 class FreestyleProjectConfigurePage {
+    getGeneralSection = () => cy.get('#general');
+    getGeneralSectionToggleBtnEnabled = () => cy.get('.jenkins-toggle-switch__label__checked-title');
+    getGeneralSectionToggleBtnDisabled = () => cy.get('.jenkins-toggle-switch__label__unchecked-title');
+    getGeneralSectionToggleBtn = () => cy.get('#toggle-switch-enable-disable-project');
     getSourceCodeManagementSection = () => cy.get('#source-code-management');
     getSrcCodeMngmntNoneOption = () => cy.get('#radio-block-0');
     getSourceCodeManagementSectionBtns = () => cy.get('#source-code-management').parent().find('[id^=radio-block-]');
     getSourceCodeManagementSectionLbls = () => cy.get('#source-code-management').parent().find('label[for^=radio-block-]');
 
+    clickGeneralSectionToggleBtn() {
+        this.getGeneralSectionToggleBtn().click();
+        return this;
+    } 
+    
     clickSourceCodeManagementMenuItem() {
         this.getSourceCodeManagementSection().click();
         return this;
