090efea057f73a44b5d99862536c79bc5424d3a3
Oxana Reshes <92902638+ReshesOxana@users.noreply.github.com>
2023-11-26 20:58:24 -0800

    RF-POM| TC_02.04.022| Verify side menu links  (#331)
    * add homePageTest
    
    * ignored old tests
    
    * resolving_homePagere_conflict
    
    * resolving conflict HomePage
    
    * skip newItemTest

--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -135,6 +135,15 @@ class HomePage {
 
     return new ParticipatePage();
   }
+
+  clickSideMenuItemList(itemName, index){
+    this.getSideMenuPanel().eq(index).as('item')
+    cy.get('@item').contains(itemName)
+    cy.get('@item').click()
+
+    return cy.url()
+
+  }
 }
 
 export default HomePage;
