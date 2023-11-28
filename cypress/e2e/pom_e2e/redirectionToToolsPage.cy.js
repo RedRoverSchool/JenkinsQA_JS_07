c5f076d6b4d3dd1b304f57904d02e51fb7c65a87
Oxana Reshes <oxanareshes@gmail.com>
2023-11-27 16:50:29 -0800

    renamed and deleted unused data

--- cypress/e2e/pom_e2e/redirectionToToolsPage.cy.js
+++ cypress/e2e/pom_e2e/redirectionToToolsPage.cy.js
@@ -1,14 +1,14 @@
 /// <reference types="cypress"/>
 import HomePage from "../../pageObjects/HomePage";
-import configureToolsTitle from "../../fixtures/pom_fixtures/configureToolsTitle.json"
+import configureToolsData from "../../fixtures/pom_fixtures/configureToolsData.json"
 
-describe('System Configuration > redirection to Tools Page', () => {
+describe('redirection to Tools Page', () => {
   const homePage = new HomePage();
 
   it('TC__09.06.02 | Verify redirection to Tools Page', () => {
     homePage.clickManageJenkinsLink()
       .clickToolsLink()
       .getToolsTitle()
-      .should('have.text', configureToolsTitle.Title)
+      .should('have.text', configureToolsData.Title)
   })
 })
