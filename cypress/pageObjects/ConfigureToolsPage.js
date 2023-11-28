769333ce2f38d83261803d333bca52ea80b3cca4
RuslanG13 <ruslan.gudenko13@ukr.net>
2023-11-28 08:47:31 +0200

    refactoring createConfigureURL method

--- cypress/pageObjects/ConfigureToolsPage.js
+++ cypress/pageObjects/ConfigureToolsPage.js
@@ -2,8 +2,8 @@ class ConfigureToolsPage {
     getConfigureToolsMainHeader = () => cy.get(".jenkins-app-bar__content h1");
     getConfigureToolsURL = () => cy.url();
 
-    createConfigureURL(baseUrl, url, rout) {
-        return `${baseUrl}${url}${rout}/`;
+    createConfigureURL(baseUrl, manageJenkinsPageEndpoint, manageJenkinsPageEndpoint) {
+        return `${baseUrl}${manageJenkinsPageEndpoint}${configureToolsEndpoint}/`;
     }
 }
 export default ConfigureToolsPage;
