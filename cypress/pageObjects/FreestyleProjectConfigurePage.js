a32bd29aa2e1820ad6fdeb794d8b633d809b1280
Vladimir Vakulenko <vakulenko.volodia@yandex.ru>
2023-12-01 00:43:50 +0200

    Added refactoring of test Creation Multiconfiguration Project 30.11.23

--- cypress/pageObjects/FreestyleProjectConfigurePage.js
+++ cypress/pageObjects/FreestyleProjectConfigurePage.js
@@ -7,29 +7,9 @@ class FreestyleProjectConfigurePage {
     getSrcCodeMngmntNoneOption = () => cy.get('#radio-block-0');
     getSourceCodeManagementSectionBtns = () => cy.get('#source-code-management').parent().find('[id^=radio-block-]');
     getSourceCodeManagementSectionLbls = () => cy.get('#source-code-management').parent().find('label[for^=radio-block-]');
-<<<<<<< HEAD
     getConfigurePageHeader = () => cy.get('h1');
     getBuildEnvironmet = () => cy.get('button[data-section-id="build-environment"]');
 
-=======
-    getGitOptionTooltip = () => cy.get('a[tooltip="Help for feature: Git"]');
-    getOptionTooltipContent = () => cy.get('div.tippy-box');
-    getGitOptionTooltipHelpArea = () => cy.get('[ref="radio-block-1"] + div.help-area > div.help');
-    getBuildEnvironmentSectionMenuItem = () => cy.get('button[data-section-id="build-environment"]');
-    getBuildEnvironmentSection = () => cy.get('#build-environment');
-    getBuildEnvironmentSectionOptions = () => cy.get(' #build-environment~.optionalBlock-container');
-    getUseSecretTextOptionTooltip = () => cy.get('a[helpurl="/descriptor/org.jenkinsci.plugins.credentialsbinding.impl.SecretBuildWrapper/help"]');
-    getListOfBuildTriggerOptions = () => cy.get("#build-triggers").parent().find('.optionalBlock-container');
-    getOneItemOfListBuildTriggerOption = (item) => item.find('.optional-block-start label.attach-previous');
-    getOneHintIconFromBuildTriggerList = (item) => item.find(".jenkins-checkbox-help-wrapper a ");
-    getHintIconTip = () => cy.get('div[id*="tippy"]')
-    
-    clickGeneralSectionToggleBtn() {
-        this.getGeneralSectionToggleBtn().click();
-        return this;
-    } 
-    
->>>>>>> 64bc9be11ad8c4238945a95169fa9ace0dfecdbe
     clickSourceCodeManagementMenuItem() {
         this.getSourceCodeManagementSection().click();
         return this;
@@ -40,27 +20,9 @@ class FreestyleProjectConfigurePage {
         return this;
     }
 
-<<<<<<< HEAD
     clickBuildEnvironment() {
         this.getBuildEnvironmet().click();
         return this;
     }
-=======
-    clickBuildEnvironmentSectionMenuItem() {
-        this.getBuildEnvironmentSectionMenuItem().click();
-        return this;
-    };
-
-    hoverUseSecretTextOptionTooltip() {
-        this.getUseSecretTextOptionTooltip().realHover();    
-        return this;
-      }
-    
-    hoverOneOfHintIconFromBuildTriggersList(item) {
-        this.getOneHintIconFromBuildTriggerList(item).realHover();
-        return this; 
-    }
-  
->>>>>>> 64bc9be11ad8c4238945a95169fa9ace0dfecdbe
 }
 export default FreestyleProjectConfigurePage;
