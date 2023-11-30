8a0f152fb0d27a0abb9dae175719421b4533fa2f
Vladimir Vakulenko <vakulenko.volodia@yandex.ru>
2023-11-30 23:44:35 +0200

    Added refactoring of test Creation Multiconfiguration Project 303.11.23

---
+++ cypress/e2e/pom_e2e/checkSecretTextInFreestyleProjectConfig.cy.js
@@ -0,0 +1,29 @@
+///<reference types = "cypress"/>
+
+import HomePage from "../../pageObjects/HomePage";
+import freestyleProjectData from "../../fixtures/pom_fixtures/freestyleProjectData.json";
+import FreestyleProjectConfigurePage from "../../pageObjects/FreestyleProjectConfigurePage";
+
+describe('Check secret text', function() {
+    const homePage = new HomePage();
+    const freestyleProjectConfigurePage = new FreestyleProjectConfigurePage();    
+
+    beforeEach(function() {
+        homePage.clickNewItemLink()
+                .fillInputNameField(freestyleProjectData.projectName)
+                .clickFreestyleTypeOfProjectBtn()
+                .clickOKButtonFreestyle()
+                // .getConfigurePageHeader().should('be.visible').and('have.text', freestyleProjectData.configure.configureHeader)  
+          
+    })
+
+    it('TC_04.04.006 | Check Secret Text In Freestyle Project Configuration', function() {
+        
+        freestyleProjectConfigurePage.clickBuildEnvironment()
+        // cy.get('button[data-section-id="build-environment"]').click()
+        cy.get('section input[id*="cb2"]').should('have.length', 6).then(($els) => {
+            // const requiredItems = Cypress.$.makeArray($els).map(($el) => $el.innerText.replace(/\n/g, '').trim());   
+
+        });
+    })
+})
--- cypress/pageObjects/FreestyleProjectConfigurePage.js
+++ cypress/pageObjects/FreestyleProjectConfigurePage.js
@@ -2,9 +2,16 @@ class FreestyleProjectConfigurePage {
     getSourceCodeManagementSection = () => cy.get('#source-code-management');
     getSourceCodeManagementSectionBtns = () => cy.get('#source-code-management').parent().find('[id^=radio-block-]');
     getSourceCodeManagementSectionLbls = () => cy.get('#source-code-management').parent().find('label[for^=radio-block-]');
+    getConfigurePageHeader = () => cy.get('h1');
+    getBuildEnvironmet = () => cy.get('button[data-section-id="build-environment"]');
 
     clickSourceCodeManagementMenuItem() {
         this.getSourceCodeManagementSection().click();
+        return this;
+    }
+
+    clickBuildEnvironment() {
+        this.getBuildEnvironmet().click();
         return this;
     }
 }
