1f1d4ce9fa3230db1c4337947b86fde5ca7ab3c3
SvetlanaKazakova <107031108+SvetlanaKazakova@users.noreply.github.com>
2023-11-30 16:31:27 +0300

    Verify size of project table S,M,L (#372)

--- cypress/e2e/pom_e2e/pipelineProject.cy.js
+++ cypress/e2e/pom_e2e/pipelineProject.cy.js
@@ -2,6 +2,7 @@
 import HomePage from "../../pageObjects/HomePage";
 import PipelineConfigurePage from "../../pageObjects/PipelineConfigurePage";
 import pipelineProjectData from "../../fixtures/pom_fixtures/pipelineProjectData.json";
+import{sizeValue} from "../../fixtures/pom_fixtures/homePageData.json"
 
 describe('New Item > Create Pipeline Project > Configure new Pipeline project', () => {
     const homePage = new HomePage();
@@ -27,8 +28,18 @@ describe('New Item > Create Pipeline Project > Configure new Pipeline project', 
       homePage.clickDashboardBreadcrumbsLink()
               .clickButtonIconSizeSmall()
       homePage.getBuildStatusIcon()
-               .should((el,ind) => {
+               .then((el) => {
         expect(window.getComputedStyle(el[0]).getPropertyValue('--table-padding')).to.equal('0.2rem');
       }); 
-   })
+   });
+
+      it('TC_03.05.019|New Item> Create Pipeline Project|Verify size of project table S,M,L ',()=>{
+      
+         homePage.getProjectStatus().should(($el, index) => {
+            const tablePadding = Cypress.$($el).css(sizeValue.checkForTablePadding)
+            expect(tablePadding).to.eq(sizeValue.RemSize[index])
+        });
+   });       
+
 })
+
