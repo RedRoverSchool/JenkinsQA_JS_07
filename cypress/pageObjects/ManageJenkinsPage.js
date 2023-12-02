fa6c0981ed3594d9f277bc89212d66795a2bbf03
Ruslan Gudenko <47644886+RuslanG13@users.noreply.github.com>
2023-12-02 23:53:57 +0200

    RF | manageJenkinsSystemConfigurationTools | TC_09.06.005 | Visibility of title and icon 'Tools' section item verification (#381)
    * RF | manageJenkinsSystemConfigurationTools | TC_09.06.005 | Visibility of title and icon 'Tools' section item verification
    
    * syntax small fix
    
    * added cy.commands for beforeEach for Manage Jenkins Page

--- cypress/pageObjects/ManageJenkinsPage.js
+++ cypress/pageObjects/ManageJenkinsPage.js
@@ -6,7 +6,7 @@ class ManageJenkinsPage {
   getHeaderManageJenkins = () => cy.get('.jenkins-app-bar h1');
   getSubHeadersManageJenkins = () => cy.get(".jenkins-section h2");
   getSystemConfigurationSection = (headerSection) => cy.get("section.jenkins-section--bottom-padding").contains(headerSection);
-  getToolsTitleSectionItem = () => cy.get("a[href='configureTools'] dl dt");
+  getToolsSectionItem = () => cy.get("a[href='configureTools'] dl dt");
   getToolsIconSectionItem = () => cy.get("a[href='configureTools'] .jenkins-section__item__icon");
   getManageJenkinsURL = () => cy.url();
 
@@ -17,7 +17,7 @@ class ManageJenkinsPage {
   }
 
   clickConfigureToolsSectionItem() {
-    this.getToolsTitleSectionItem().should("be.visible").click();
+    this.getToolsSectionItem().should("be.visible").click();
 
     return new ConfigureToolsPage();
   }
@@ -26,6 +26,13 @@ class ManageJenkinsPage {
     return this.getSubHeadersManageJenkins().should("be.visible")
       .then(($els) => {
         return Cypress.$.makeArray($els).map($el => $el.innerText);
+      });
+  }
+
+  retrieveToolsSectionHeader() {
+    return this.getToolsSectionItem().should("be.visible")
+      .then(($el) => {
+        $el.text();
       });
   }
 }
