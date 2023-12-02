73a0218ecb8f43973a4de8a0fc3826c9fa03b61d
Oxana Reshes <oxanareshes@gmail.com>
2023-12-01 16:03:30 -0800

    delete line #4

--- cypress/pageObjects/CreateItemErrorPage.js
+++ cypress/pageObjects/CreateItemErrorPage.js
@@ -1,7 +1,7 @@
 import newProject from "../fixtures/pom_fixtures/newProject.json"
 class CreateItemErrorPage{
  getMessage = ()=> cy.get('h1')
-                     .should('have.text', newProject.message)
+                   
  }
 
 export default CreateItemErrorPage
