c2e429f8f4a0e69f8d3aa837cf45af6c0c9e8e5f
Alexey <bainoff@gmail.com>
2023-12-03 23:05:18 +0100

    added 02.05 check Build status blocks commit 2

--- cypress/e2e/pom_e2e/buildsInformationCheck.cy.js
+++ cypress/e2e/pom_e2e/buildsInformationCheck.cy.js
@@ -11,23 +11,4 @@ describe("US_02.05 | Dashboard -> Information about User's builds", () => {
       .should("contain", buildsInformation.buildQueueBlockTitle)
       .and("be.visible");
   });
-
-  it("TC_02.05.002 | Dashboard > Information about User's builds > Check that 'Build queue' block is able to expand/collapse", () => {
-    homePage.takeBuildQueueBlockState().then((blockState) => {
-      switch (blockState) {
-        case buildsInformation.buildQueueBlockState.collapsed:
-          homePage
-            .clickBuildQueueBlockArrow()
-            .getBuildQueueBlockContent()
-            .should("not.have.text", buildsInformation.buildQueueAbsenceMsg);
-          break;
-        case buildsInformation.buildQueueBlockState.expanded:
-          homePage
-            .clickBuildQueueBlockArrow()
-            .getBuildQueueBlockContent()
-            .should("have.text", buildsInformation.buildQueueAbsenceMsg);
-          break;
-      }
-    });
-  });
 });
