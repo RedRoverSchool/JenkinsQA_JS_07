57f82b26ee913c2b7d9cd46724f437bdf819e1ad
RuslanG13 <ruslan.gudenko13@ukr.net>
2023-12-06 00:06:40 +0200

    RF | POM > TC_03.07.003 | New Item > Create Multiconfiguration project > Disable project

--- cypress/pageObjects/MultiConfigProjectConfigurePage.js
+++ cypress/pageObjects/MultiConfigProjectConfigurePage.js
@@ -1,10 +1,14 @@
+import MultiConfigProjectPage from "./MultiConfigProjectPage";
+
 class MultiConfigProjectConfigurePage {
     getConfigSection = () => cy.get('.jenkins-app-bar__content > h1');
     getSideTasksMenu = () => cy.get('div#tasks .task');
     getSidePanel = () => cy.get('#side-panel');
     getDescriptionTextArea = () => cy.get("textarea[name='description']");
     getSaveButton = () => cy.get("button[name='Submit']");
-    getCreatedDescription = () => cy.get("#description>div:first-child");
+    getEnabledProjectLabel = () => cy.get("span.jenkins-toggle-switch__label__checked-title");
+    getDisabledProjectLabel = () => cy.get("span.jenkins-toggle-switch__label__unchecked-title");
+    getToggleSwitchButton = () => cy.get("label.jenkins-toggle-switch__label ");
 
     typeValueToTheDescrioinTextArea(value) {
         this.getDescriptionTextArea().type(value);
@@ -12,10 +16,18 @@ class MultiConfigProjectConfigurePage {
         return this;
     }
 
+    disableMultiConfigProject(enabledProject, disabledProject) {
+        this.getEnabledProjectLabel().should("have.text", enabledProject);
+        this.getToggleSwitchButton().click();
+        this.getDisabledProjectLabel().should("have.text", disabledProject);
+
+        return this;
+    }
+
     clickSaveButton() {
         this.getSaveButton().click();
 
-        return this;
+        return new MultiConfigProjectPage();
     }
 }
 export default MultiConfigProjectConfigurePage;
