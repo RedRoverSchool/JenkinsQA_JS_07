713bb458f7ee254474485b7027d1a9b6f2f28c27
IuliiaGera <sckrafftykt@gmail.com>
2023-12-07 20:17:46 -0600

    TC_02.02.013 | Header > Verify Jenkins Logo is visible and clickable

--- cypress/pageObjects/PeoplePage.js
+++ cypress/pageObjects/PeoplePage.js
@@ -2,11 +2,19 @@ import HomePage from "../pageObjects/HomePage";
 
 class PeoplePage {
     getJenkinsHomeLink = () => cy.get('#jenkins-home-link');
+    getJenkinsLogo = () => cy.get('#jenkins-head-icon')
 
     clickJenkinsHomeLink() {
         this.getJenkinsHomeLink().click()
 
         return new HomePage();
     }
+
+    clickJenkinsLogo() {
+        this.getJenkinsLogo().should('be.visible').click()
+
+        return new HomePage()
+    }
+
 }
 export default PeoplePage;
