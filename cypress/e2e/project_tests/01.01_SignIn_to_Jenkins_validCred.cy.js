4e3393504dca7f8b27a45485e5e5bae107e4f948
Hanna <annpetlitskaya@gmail.com>
2023-12-20 16:03:17 +0100

    delete const from valid cred doc

--- cypress/e2e/project_tests/01.01_SignIn_to_Jenkins_validCred.cy.js
+++ cypress/e2e/project_tests/01.01_SignIn_to_Jenkins_validCred.cy.js
@@ -1,11 +1,6 @@
 
 import {WelcomeMessage} from "../../fixtures/Project_test_data/01.01_SignIn_to_Jenkins.json"
 
-const username = Cypress.env("local.admin.username")
-const password = Cypress.env("local.admin.password")
-const localhost = Cypress.env("local.host")
-const localport = Cypress.env("local.port")
-
 describe('01.01_SignIn_to_Jenkins: valid credentials', () => {
 
     it('valid credentials', () => {
